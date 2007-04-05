cc2160021abb6faf2a4305affefc345ed78d1336
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-05 18:28:02 +0000

    More minor comment tweaks Added header examples
    git-svn-id: http://code.extjs.com/svn/ext/trunk@40 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/MessageBox.js
+++ src/widgets/MessageBox.js
@@ -1,6 +1,27 @@
 /**
  * @class Ext.MessageBox
  * Utility class for generating different styles of message boxes.  The alias Ext.Msg can also be used.
+ * Example usage:
+ *<pre><code>
+// Basic alert:
+Ext.Msg.alert('Status', 'Changes saved successfully.');
+
+// Prompt for user data:
+Ext.Msg.prompt('Name', 'Please enter your name:', function(btn, text){
+    if (btn == 'ok'){
+        // process text value...
+    }
+});
+
+// Show a dialog using config options:
+Ext.Msg.show({
+   title:'Save Changes?',
+   msg: 'Your are closing a tab that has unsaved changes. Would you like to save your changes?',
+   buttons: Ext.Msg.YESNOCANCEL,
+   fn: processResult,
+   animEl: 'elId'
+});
+ </code></pre>
  * @singleton
  */
 Ext.MessageBox = function(){
