13c9c4bffd6a7ea6803bb219339acc5fd46871db
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-07 07:35:48 +0000

    Added docs
    git-svn-id: http://code.extjs.com/svn/ext/trunk@60 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/form/TriggerField.js
+++ src/widgets/form/TriggerField.js
@@ -1,3 +1,24 @@
+/**
+ * @class Ext.form.TriggerField
+ * @extends Ext.form.TextField
+ * Provides a convenient wrapper for TextFields that adds a clickable trigger button (looks like a combobox by default).
+ * The trigger has no default action, so you must assign a function to implement the trigger click handler by
+ * overriding {@link #onTriggerClick}. You can create a TriggerField directly, as it renders exactly like a combobox
+ * for which you can provide a custom implementation.  For example:
+ * <pre><code>
+var trigger = new Ext.form.TriggerField();
+trigger.onTriggerClick = myTriggerFn;
+trigger.applyTo('my-field');
+</code></pre>
+ *
+ * However, in general you will most likely want to use TriggerField as the base class for a reusable component.
+ * {@link Ext.form.DateField} and {@link Ext.form.ComboBox} are perfect examples of this.
+ * @cfg {String} triggerClass The CSS class used to style the trigger button
+ * @constructor
+ * Create a new TriggerField.
+ * @param {Object} config Configuration options (valid {@Ext.form.TextField} config options will also be applied
+ * to the base TextField)
+ */
 Ext.form.TriggerField = function(config){
     Ext.form.TriggerField.superclass.constructor.call(this, config);
     this.mimicing = false;
@@ -9,7 +30,9 @@ Ext.extend(Ext.form.TriggerField, Ext.form.TextField,  {
     defaultAutoCreate : {tag: "input", type: "text", size: "16", autocomplete: "off"},
     customSize : true,
     hideTrigger:false,
-    
+
+
+    // private
     setSize : function(w, h){
         if(!this.wrap){
             this.width = w;
@@ -30,6 +53,7 @@ Ext.extend(Ext.form.TriggerField, Ext.form.TextField,  {
         }
     },
 
+    // private
     onRender : function(ct){
         Ext.form.TriggerField.superclass.onRender.call(this, ct);
         this.wrap = this.el.wrap({cls: "x-form-field-wrap"});
@@ -44,6 +68,7 @@ Ext.extend(Ext.form.TriggerField, Ext.form.TextField,  {
         this.setSize(this.width||'', this.height||'');
     },
 
+    // private
     onFocus : function(){
         Ext.form.TriggerField.superclass.onFocus.call(this);
         if(!this.mimicing){
@@ -53,22 +78,26 @@ Ext.extend(Ext.form.TriggerField, Ext.form.TextField,  {
         }
     },
 
+    // private
     checkTab : function(e){
         if(e.getKey() == e.TAB){
             this.triggerBlur();
         }
     },
 
+    // private
     onBlur : function(){
         // do nothing
     },
 
+    // private
     mimicBlur : function(e, t){
         if(!this.wrap.contains(t) && this.validateBlur()){
             this.triggerBlur();
         }
     },
 
+    // private
     triggerBlur : function(){
         this.mimicing = false;
         Ext.get(document).un("mousedown", this.mimicBlur);
@@ -76,21 +105,31 @@ Ext.extend(Ext.form.TriggerField, Ext.form.TextField,  {
         Ext.form.TriggerField.superclass.onBlur.call(this);
     },
 
+    /**
+     * This should be overriden by any subclass that needs to check whether or not the field can be blurred.
+     * @param event The event to check
+     * @param target The event target
+     */
     validateBlur : function(e, t){
         return true;
     },
 
+    // private
     disableWrapper : function(){
         if(this.wrap){
             this.wrap.addClass('x-item-disabled');
         }
     },
 
+    // private
     enableWrapper : function(){
         if(this.wrap){
             this.wrap.removeClass('x-item-disabled');
         }
     },
 
+    /**
+     * The function that should handle the triggerclick event.  This method does nothing by default until a handler is assigned.
+     */
     onTriggerClick : Ext.emptyFn
 });
