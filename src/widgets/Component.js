0fd829f12eba2b6ae7cac2507399deeb6a989d16
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-08 08:19:38 +0000

    Added docs
    git-svn-id: http://code.extjs.com/svn/ext/trunk@72 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/Component.js
+++ src/widgets/Component.js
@@ -1,19 +1,36 @@
+/**
+ * @class Ext.ComponentMgr
+ * Provides a common registry of all components on a page so that they can be easily accessed by component id.
+ * @singleton
+ */
 Ext.ComponentMgr = function(){
     var all = new Ext.util.MixedCollection();
 
     return {
+        // private
         register : function(c){
             all.add(c);
         },
 
+        // private
         unregister : function(c){
             all.remove(c);
         },
 
+        /**
+         * Returns a component by id
+         * @param {String} id The component id
+         */
         get : function(id){
             return all.get(id);
         },
 
+        /**
+         * Registers a function that will be called when a specified component is added to ComponentMgr
+         * @param {String} id The component id
+         * @param {Funtction} fn The callback function
+         * @param {Object} scope The scope of the callback
+         */
         onAvailable : function(id, fn, scope){
             all.on("add", function(index, o){
                 if(o.id == id){
@@ -25,6 +42,13 @@ Ext.ComponentMgr = function(){
     };
 }();
 
+/**
+ * @class Ext.Component
+ * Base class for all Ext form controls that provides a common set of events and functionality shared by all components.
+ * @cfg {Ext.Element/String/Object} config The configuration options.  If an element is passed, it is set as the internal
+ * element and its id used as the component id.  If a string is passed, it is assumed to be the id of an existing element
+ * and is used as the component id.  Otherwise, it is assumed to be a standard config object and is applied to the component.
+ */
 Ext.Component = function(config){
     config = config || {};
     if(config.tagName || config.dom || typeof config == "string"){ // element object
@@ -32,15 +56,65 @@ Ext.Component = function(config){
     }
     Ext.apply(this, config);
     this.addEvents({
+        /**
+         * @event disable
+         * Fires after the component is disabled
+	     * @param {Ext.Component} this
+	     */
         disable : true,
+        /**
+         * @event enable
+         * Fires after the component is enabled
+	     * @param {Ext.Component} this
+	     */
         enable : true,
+        /**
+         * @event beforeshow
+         * Fires before the component is shown
+	     * @param {Ext.Component} this
+	     */
         beforeshow : true,
+        /**
+         * @event show
+         * Fires after the component is shown
+	     * @param {Ext.Component} this
+	     */
         show : true,
+        /**
+         * @event beforehide
+         * Fires before the component is hidden
+	     * @param {Ext.Component} this
+	     */
         beforehide : true,
+        /**
+         * @event hide
+         * Fires after the component is hidden
+	     * @param {Ext.Component} this
+	     */
         hide : true,
+        /**
+         * @event beforerender
+         * Fires before the component is rendered
+	     * @param {Ext.Component} this
+	     */
         beforerender : true,
+        /**
+         * @event render
+         * Fires after the component is rendered
+	     * @param {Ext.Component} this
+	     */
         render : true,
+        /**
+         * @event beforedestroy
+         * Fires before the component is destroyed
+	     * @param {Ext.Component} this
+	     */
         beforedestroy : true,
+        /**
+         * @event destroy
+         * Fires after the component is destroyed
+	     * @param {Ext.Component} this
+	     */
         destroy : true
     });
     if(!this.id){
@@ -49,6 +123,7 @@ Ext.Component = function(config){
     Ext.ComponentMgr.register(this);
 };
 
+// private
 Ext.Component.AUTO_ID = 1000;
 
 Ext.extend(Ext.Component, Ext.util.Observable, {
@@ -69,14 +144,18 @@ Ext.extend(Ext.Component, Ext.util.Observable, {
      */
     rendered : false,
 
+    // private
     ctype : "Ext.Component",
 
+    // private
     actionMode : "el",
 
+    // private
     getActionEl : function(){
         return this[this.actionMode];
     },
 
+    // private
     render : function(container){
         if(!this.rendered && this.fireEvent("beforerender", this) !== false){
             this.container = Ext.get(container);
@@ -97,13 +176,14 @@ Ext.extend(Ext.Component, Ext.util.Observable, {
         }
     },
 
+    // private
     // default function is not really useful
     onRender : function(ct){
         this.el = Ext.get(this.el);
         ct.dom.appendChild(this.el.dom);
     },
 
-    //@private
+    // private
     getAutoCreate : function(){
         var cfg = typeof this.autoCreate == "object" ?
                       this.autoCreate : Ext.apply({}, this.defaultAutoCreate);
@@ -113,8 +193,10 @@ Ext.extend(Ext.Component, Ext.util.Observable, {
         return cfg;
     },
 
+    // private
     afterRender : Ext.emptyFn,
 
+    // private
     destroy : function(){
         if(this.fireEvent("beforedestroy", this) !== false){
             this.purgeListeners();
@@ -130,13 +212,17 @@ Ext.extend(Ext.Component, Ext.util.Observable, {
         }
     },
 
+    /**
+     * Returns the underlying {@link Ext.Element}
+     * @return {Ext.Element} The element
+     */
     getEl : function(){
         return this.el;
     },
 
-/**
- * Try to focus this component
- */
+    /**
+     * Try to focus this component
+     */
     focus : function(selectText){
         if(this.rendered){
             this.el.focus();
@@ -146,14 +232,16 @@ Ext.extend(Ext.Component, Ext.util.Observable, {
         }
     },
 
+    // private
     blur : function(){
         if(this.rendered){
             this.el.blur();
         }
     },
-/**
- * Disable this component
- */
+
+    /**
+     * Disable this component
+     */
     disable : function(){
         if(this.rendered){
             this.getActionEl().addClass(this.disabledClass);
@@ -163,9 +251,9 @@ Ext.extend(Ext.Component, Ext.util.Observable, {
         this.fireEvent("disable", this);
     },
 
-/**
- * Enable this component
- */
+    /**
+     * Enable this component
+     */
     enable : function(){
         if(this.rendered){
             this.getActionEl().removeClass(this.disabledClass);
@@ -175,13 +263,14 @@ Ext.extend(Ext.Component, Ext.util.Observable, {
         this.fireEvent("enable", this);
     },
 
+    // private
     setDisabled : function(disabled){
         this[disabled ? "disable" : "enable"]();
     },
 
-/**
- * Show this component
- */
+    /**
+     * Show this component
+     */
     show: function(){
         if(this.fireEvent("beforeshow", this) !== false){
             this.hidden = false;
@@ -192,15 +281,16 @@ Ext.extend(Ext.Component, Ext.util.Observable, {
         }
     },
 
+    // private
     onShow : function(){
         var st = this.getActionEl().dom.style;
         st.display = "";
         st.visibility = "visible";
     },
 
-/**
- * Hide this component
- */
+    /**
+     * Hide this component
+     */
     hide: function(){
         if(this.fireEvent("beforehide", this) !== false){
             this.hidden = true;
@@ -211,10 +301,15 @@ Ext.extend(Ext.Component, Ext.util.Observable, {
         }
     },
 
+    // private
     onHide : function(){
         this.getActionEl().dom.style.display = "none";
     },
 
+    /**
+     * Hide or show this component
+     * @param {Boolean} visible True to show, false to hide
+     */
     setVisible: function(visible){
         if(visible) {
             this.show();
