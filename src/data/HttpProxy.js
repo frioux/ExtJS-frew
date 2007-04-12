1dd806eb3e804b19bf43289c35e118c75d4f54ca
nwhite <nwhite@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-12 14:02:33 +0000

    Added API docs.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@127 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/data/HttpProxy.js
+++ src/data/HttpProxy.js
@@ -1,3 +1,19 @@
+/**
+ * An implementation of Ext.data.DataProxy that reads a data object from an Ext.data.Connection object
+ * configured to reference a certain URL.
+ * <p>
+ * <em>Note that this class cannot be used to retrieve data from a domain other than the domain
+ * from which the running page was served.
+ * <p>
+ * For cross-domain access to remote data, use an Ext.data.ScriptTagProxy.
+ * </em>
+ * <p>
+ * Be aware that to enable the browser to parse an XML document, the server <strong>must</strong> set
+ * the Content-Type header to "text/xml".
+ * @constructor
+ * @param {Object} conn An Ext.data.Connection object referencing the URL from which the data object
+ * is to be read, or a configuration object for an Ext.data.Connection.
+ */
 Ext.data.HttpProxy = function(conn){
     Ext.data.HttpProxy.superclass.constructor.call(this);
     // is conn a conn config or a real conn?
@@ -5,10 +21,27 @@ Ext.data.HttpProxy = function(conn){
 };
 
 Ext.extend(Ext.data.HttpProxy, Ext.data.DataProxy, {
+    // private
     getConnection : function(){
         return this.conn;
     },
-    
+
+    /**
+     * Load data from the configured Ext.data.Connection, read the data object into
+     * a block of Ext.data.Records using the passed Ext.data.DataReader implementation, and
+     * process that block using the passed callback.
+     * @param {Object} params This parameter is not used by the MemoryProxy class.
+     * @param {Ext.data.DataReader) reader The Reader object which converts the data
+     * object into a block of Ext.data.Records.
+     * @param {Function} callback The function into which to pass the block of Ext.data.records.
+     * The function must be passed <ul>
+     * <li>The Record block object</li>
+     * <li>The "arg" argument from the load function</li>
+     * <li>A boolean success indicator</li>
+     * </ul>
+     * @param {Object} scope The scope in which to call the callback
+     * @param {Object} arg An optional argument which is passed to the callback as its second parameter.
+     */
     load : function(params, reader, callback, scope, arg){
         if(this.fireEvent("beforeload", this, params) !== false){
             this.conn.request({
@@ -27,6 +60,7 @@ Ext.extend(Ext.data.HttpProxy, Ext.data.DataProxy, {
         }
     },
     
+    // private
     loadResponse : function(o, success, response){
         if(!success){
             this.fireEvent("loadexception", this, o, response);
@@ -44,10 +78,12 @@ Ext.extend(Ext.data.HttpProxy, Ext.data.DataProxy, {
         o.request.callback.call(o.request.scope, result, o.request.arg, true);
     },
     
+    // private
     update : function(dataSet){
         
     },
     
+    // private
     updateResponse : function(dataSet){
         
     }
