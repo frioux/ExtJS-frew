dd9a78784ac39c52b94405a51128705d900d8d62
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-14 09:50:16 +0000

    Added docs
    git-svn-id: http://code.extjs.com/svn/ext/trunk@161 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/ColorPalette.js
+++ src/widgets/ColorPalette.js
@@ -1,6 +1,29 @@
+/**
+ * @class Ext.ColorPalette
+ * @extends Ext.Component
+ * Simple color palette class for choosing colors.  The palette can be rendered to any container.<br />
+ * Here's an example of typical usage:
+ * <pre><code>
+var cp = new Ext.ColorPalette({value:'993300'});  // initial selected color
+cp.render('my-div');
+
+cp.on('select', function(palette, selColor){
+    // do something with selColor
+});
+</code></pre>
+ * @constructor
+ * Create a new ColorPalette
+ * @param {Object} config The config object
+ */
 Ext.ColorPalette = function(config){
     Ext.ColorPalette.superclass.constructor.call(this, config);
     this.addEvents({
+        /**
+	     * @event select
+	     * Fires when a color is selected
+	     * @param {Button} this
+	     * @param {String} color The 6-digit color hex code (without the # symbol)
+	     */
         select: true
     });
 
@@ -9,10 +32,39 @@ Ext.ColorPalette = function(config){
     }
 };
 Ext.extend(Ext.ColorPalette, Ext.Component, {
+    /**
+     * @cfg {String} itemCls
+     * The CSS class to apply to the containing element (defaults to "x-color-palette")
+     */
     itemCls : "x-color-palette",
+    /**
+     * @cfg {String} value
+     * The initial color to highlight (should be a valid 6-digit color hex code without the # symbol).  Note that
+     * the hex codes are case-sensitive.
+     */
     value : null,
+
+    // private
     ctype: "Ext.ColorPalette",
 
+    /**
+     * <p>An array of 6-digit color hex code strings (without the # symbol).  This array can contain any number
+     * of colors, and each hex code should be unique.  The width of the palette is controlled via CSS by adjusting
+     * the width property of the 'x-color-palette' class (or assigning a custom class), so you can balance the number
+     * of colors with the width setting until the box is symmetrical.</p>
+     * <p>You can override individual colors if needed:</p>
+     * <pre><code>
+var cp = new Ext.ColorPalette();
+cp.colors[0] = "FF0000";  // change the first box to red
+</code></pre>
+
+Or you can provide a custom array of your own for complete control:
+<pre><code>
+var cp = new Ext.ColorPalette();
+cp.colors = ["000000", "993300", "333300"];
+</code></pre>
+     * @type Array
+     */
     colors : [
         "000000", "993300", "333300", "003300", "003366", "000080", "333399", "333333",
         "800000", "FF6600", "808000", "008000", "008080", "0000FF", "666699", "808080",
@@ -21,6 +73,7 @@ Ext.extend(Ext.ColorPalette, Ext.Component, {
         "FF99CC", "FFCC99", "FFFF99", "CCFFCC", "CCFFFF", "99CCFF", "CC99FF", "FFFFFF"
     ],
 
+    // private
     onRender : function(container){
         var t = new Ext.MasterTemplate(
             '<tpl><a href="#" class="color-{0}" hidefocus="on"><em><span style="background:#{0}">&#160;</span></em></a></tpl>'
@@ -37,6 +90,7 @@ Ext.extend(Ext.ColorPalette, Ext.Component, {
         this.el.on("click", this.handleClick,  this, {delegate: "a"});
     },
 
+    // private
     afterRender : function(){
         if(this.value){
             var s = this.value;
@@ -45,6 +99,7 @@ Ext.extend(Ext.ColorPalette, Ext.Component, {
         }
     },
 
+    // private
     handleClick : function(e, t){
         e.preventDefault();
         if(!this.disabled){
@@ -53,6 +108,10 @@ Ext.extend(Ext.ColorPalette, Ext.Component, {
         }
     },
 
+    /**
+     * Selects the specified color in the palette (fires the select event)
+     * @param {String} color A valid 6-digit color hex code (# will be stripped if included)
+     */
     select : function(color){
         color = color.replace("#", "");
         if(color != this.value){
