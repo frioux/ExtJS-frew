efc4342502d5d31a067459524740f61891fdff09
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-14 12:03:51 +0000

    Added docs
    git-svn-id: http://code.extjs.com/svn/ext/trunk@165 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/LoadMask.js
+++ src/widgets/LoadMask.js
@@ -1,3 +1,13 @@
+/**
+ * @class Ext.LoadMask
+ * A simple utility class for generically masking elements while loading data.  If the element being masked has
+ * an underlying {@link Ext.data.Store}, the masking will be automatically synchronized with the store's loading
+ * process and the mask element will be cached for reuse.  For all other elements, this mask will replace the
+ * element's UpdateManager load indicator and will be destroyed after the initial load.
+ * @constructor
+ * Create a new LoadMask
+ * @param {Object} config The config object
+ */
 Ext.LoadMask = function(el, config){
     this.el = Ext.get(el);
     Ext.apply(this, config);
@@ -17,28 +27,55 @@ Ext.LoadMask = function(el, config){
 };
 
 Ext.LoadMask.prototype = {
+    /**
+     * @cfg {Boolean} removeMask
+     * True to create a single-use mask that is automatically destroyed after loading (useful for page loads),
+     * False to persist the mask element reference for multiple uses (e.g., for paged data widgets).  Defaults to false.
+     */
+    /**
+     * @cfg {String} msg
+     * The text to display in a centered loading message box (defaults to 'Loading...')
+     */
     msg : 'Loading...',
+    /**
+     * @cfg {String} msgCls
+     * The CSS class to apply to the loading message element (defaults to "x-mask-loading")
+     */
     msgCls : 'x-mask-loading',
+
+    /**
+     * Read-only. True if the mask is currently disabled so that it will not be displayed (defaults to false)
+     * @type Boolean
+     */
     disabled: false,
 
+    /**
+     * Disables the mask to prevent it from being displayed
+     */
     disable : function(){
        this.disabled = true;
     },
 
+    /**
+     * Enables the mask so that it can be displayed
+     */
     enable : function(){
         this.disabled = false;
     },
 
+    // private
     onLoad : function(){
         this.el.unmask(this.removeMask);
     },
 
+    // private
     onBeforeLoad : function(){
         if(!this.disabled){
             this.el.mask(this.msg, this.msgCls);
         }
     },
 
+    // private
     destroy : function(){
         if(this.store){
             this.store.un('beforeload', this.onBeforeLoad, this);
