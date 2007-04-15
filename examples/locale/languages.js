1bcbf5d413eb262ab4e05f7155024ecffa51a88b
trbs <trbs@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-15 03:59:54 +0000

    languages.js: added more languages dutch-form.html: changed title to match example
    git-svn-id: http://code.extjs.com/svn/ext/trunk@178 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/locale/languages.js
+++ examples/locale/languages.js
@@ -1,13 +1,22 @@
 // some data used in the examples
 Ext.namespace('Ext.exampledata');
 
-// TODO: complete the list
+// TODO: complete and sort the list
 Ext.exampledata.languages = [
 	['en', 'English'],
+	['de', 'German'],
+	['da', 'Danish'],
 	['nl', 'Dutch'],
 	['ru', 'Russian'],
 	['ja', 'Japanese'],
 	['it', 'Italian'],
 	['pt_br', 'Portuguese'],
-	['fr_CA', 'France']
+	['hu', 'Hungarian'],
+	['hr', 'Croatian'],
+	['fr_CA', 'France'],
+	['sp', 'Spanish'],
+	['tr', 'Turkish'],
+	['vn', 'Vietnamese'],
+	['zh_TW', 'Traditional Chinese'],
+	['zh_CN', 'Simplified Chinese']
     ];
