449cf217cc1bb88e82bbaec1c24659dd2112c47e
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-15 04:43:00 +0000

    Added docs, but still needs review
    git-svn-id: http://code.extjs.com/svn/ext/trunk@181 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/dd/Registry.js
+++ src/dd/Registry.js
@@ -1,3 +1,8 @@
+/**
+ * @class Ext.dd.Registry
+ * Provides easy access to all drag drop components that are registered on a page.
+ * @singleton
+ */
 Ext.dd.Registry = function(){
     var elements = {}; 
     var handles = {}; 
@@ -16,6 +21,18 @@ Ext.dd.Registry = function(){
     };
     
     return {
+    /**
+     * Resgister a drag drop element
+     * @param {String/HTMLElement) element The id or DOM node to register
+     * @param {Object} data An object with one or more of the following values:
+     * <pre>
+Value      Description
+---------  --------------------------------------
+node       
+handles
+isHandle
+</pre>
+     */
         register : function(el, data){
             data = data || {};
             if(typeof el == "string"){
@@ -33,7 +50,11 @@ Ext.dd.Registry = function(){
                 }
             }
         },
-        
+
+    /**
+     * Unregister a drag drop element
+     * @param {String/HTMLElement) element The id or DOM node to unregister
+     */
         unregister : function(el){
             var id = getId(el, false);
             var data = elements[id];
@@ -47,26 +68,42 @@ Ext.dd.Registry = function(){
                 }
             }
         },
-        
+
+    /**
+     * Returns the drag handle registered for the specified id
+     * @return {HTMLElement} handle The drag handle
+     */
         getHandle : function(id){
             if(typeof id != "string"){ // must be element?
                 id = id.id;
             }
             return handles[id];
         },
-        
+
+    /**
+     * Returns the drag handle that is the target of the specified event
+     * @return {HTMLElement} handle The drag handle
+     */
         getHandleFromEvent : function(e){
             var t = Ext.lib.Event.getTarget(e);
             return t ? handles[t.id] : null;
         },
-        
+
+    /**
+     * Returns the drop target registered for the specified id
+     * @return {HTMLElement} target The drop target
+     */
         getTarget : function(id){
             if(typeof id != "string"){ // must be element?
                 id = id.id;
             }
             return elements[id];
         },
-        
+
+    /**
+     * Returns the drop target that is the target of the specified event
+     * @return {HTMLElement} target The drop target
+     */
         getTargetFromEvent : function(e){
             var t = Ext.lib.Event.getTarget(e);
             return t ? elements[t.id] || handles[t.id] : null;
