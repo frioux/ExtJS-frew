304e05d94cd39c8ec8e2a9be101765f98156fb16
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-15 09:52:05 +0000

    Updated docs
    git-svn-id: http://code.extjs.com/svn/ext/trunk@185 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/dd/DropTarget.js
+++ src/dd/DropTarget.js
@@ -27,12 +27,12 @@ Ext.extend(Ext.dd.DropTarget, Ext.dd.DDTarget, {
      * (defaults to "").
      */
     /**
-     * @cfg {String} dropAllowed The CSS class applied to the drop target element when drop is allowed
+     * @cfg {String} dropAllowed The CSS class returned to the drag source when drop is allowed
      * (defaults to "x-dd-drop-ok").
      */
     dropAllowed : "x-dd-drop-ok",
     /**
-     * @cfg {String} dropNotAllowed The CSS class applied to the drop target element when drop is not allowed
+     * @cfg {String} dropNotAllowed The CSS class returned to the drag source when drop is not allowed
      * (defaults to "x-dd-drop-nodrop").
      */
     dropNotAllowed : "x-dd-drop-nodrop",
@@ -44,9 +44,9 @@ Ext.extend(Ext.dd.DropTarget, Ext.dd.DDTarget, {
     isNotifyTarget : true,
 
     /**
-     * Notifies the drop target once that a {@link Ext.dd.DragSource} has been dragged over it.  This default
-     * implementation adds the CSS class specified by overClass (if any) to the drop element and returns
-     * the dropAllowed config value.  This method should be overridden if drop validation is required.
+     * The function a {@link Ext.dd.DragSource} calls once to notify this drop target that it is now over the
+     * target.  This default implementation adds the CSS class specified by overClass (if any) to the drop element
+     * and returns the dropAllowed config value.  This method should be overridden if drop validation is required.
      * @param {Ext.dd.DDProxy} ddProxy The proxy that was dragged over this drop target
      * @param {Event} e The event
      * @paramn {Object} data An object containing arbitrary data supplied by the drag source
@@ -61,7 +61,7 @@ Ext.extend(Ext.dd.DropTarget, Ext.dd.DDTarget, {
     },
 
     /**
-     * Notifies the drop target continuously that a {@link Ext.dd.DragSource} is being dragged over it.
+     * The function a {@link Ext.dd.DragSource} calls continuously while it is being dragged over the target.
      * This method will be called on every mouse movement while the drag source is over the drop target.
      * This default implementation simply returns the dropAllowed config value.
      * @param {Ext.dd.DDProxy} ddProxy The proxy that was dragged over this drop target
@@ -75,8 +75,9 @@ Ext.extend(Ext.dd.DropTarget, Ext.dd.DDTarget, {
     },
 
     /**
-     * Notifies the drop target once that a {@link Ext.dd.DragSource} has been dragged out of it.  This default
-     * implementation simply removes the CSS class specified by overClass (if any) from the drop element.
+     * The function a {@link Ext.dd.DragSource} calls once to notify this drop target that it has been dragged out
+     * pf the target without dropping.  This default implementation simply removes the CSS class specified by
+     * overClass (if any) from the drop element.
      * @param {Ext.dd.DDProxy} ddProxy The proxy that was dragged over this drop target
      * @param {Event} e The event
      * @paramn {Object} data An object containing arbitrary data supplied by the drag source
@@ -88,9 +89,10 @@ Ext.extend(Ext.dd.DropTarget, Ext.dd.DDTarget, {
     },
 
     /**
-     * Notifies the drop target that a {@link Ext.dd.DragSource} has been dropped on it.  This method has no
-     * default implementation and returns false, so you must provide an implementation that does something to
-     * process the drop event and returns true so that the drag source's repair action does not run.
+     * The function a {@link Ext.dd.DragSource} calls once to notify this drop target that the dragged item has
+     * been dropped on it.  This method has no default implementation and returns false, so you must provide an
+     * implementation that does something to process the drop event and returns true so that the drag source's
+     * repair action does not run.
      * @param {Ext.dd.DDProxy} ddProxy The proxy that was dragged over this drop target
      * @param {Event} e The event
      * @paramn {Object} data An object containing arbitrary data supplied by the drag source
