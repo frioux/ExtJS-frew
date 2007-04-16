2cc872936b2ba93fba248c5283aadfd9fa09ae94
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-16 02:20:55 +0000

    Updated docs
    git-svn-id: http://code.extjs.com/svn/ext/trunk@198 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/form/Radio.js
+++ src/widgets/form/Radio.js
@@ -1,7 +1,8 @@
 /**
  * @class Ext.form.Radio
  * @extends Ext.form.Checkbox
- * Single radio field.
+ * Single radio field.  Same as Checkbox, but provided as a convenience for automatically setting the input type.
+ * Radio grouping is handled automatically by the browser if you give each radio in a group the same name.
  * @constructor
  * Creates a new Radio
  * @param {Object} config Configuration options
