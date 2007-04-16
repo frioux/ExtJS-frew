f8a16e035ecdbeeaaee0c53de9f713ba5f7bdc1f
trbs <trbs@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-16 00:46:00 +0000

    revised spanish translation by efege
    git-svn-id: http://code.extjs.com/svn/ext/trunk@195 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/locale/ext-lang-sp.js
+++ src/locale/ext-lang-sp.js
@@ -1,6 +1,6 @@
 /*
- * Español/Latinoamerica Translation by genius551v
- * 04-08-2007, 05:58 AM
+ * Español/Latinoamerica Translation by genius551v 04-08-2007
+ * Revised by efege, 2007-04-15.
  */
 
 Ext.UpdateManager.defaults.indicatorText = '<div class="loading-indicator">Cargando...</div>';
@@ -10,15 +10,15 @@ if(Ext.View){
 }
 
 if(Ext.grid.Grid){
-   Ext.grid.Grid.prototype.ddText = "{0} fila(s) seleccionadas";
+   Ext.grid.Grid.prototype.ddText = "{0} fila(s) seleccionada(s)";
 }
 
 if(Ext.TabPanelItem){
-   Ext.TabPanelItem.prototype.closeText = "Cerrar este tab";
+   Ext.TabPanelItem.prototype.closeText = "Cerrar este pestaña";
 }
 
 if(Ext.form.Field){
-   Ext.form.Field.prototype.invalidText = "El valor en este campo es invalido";
+   Ext.form.Field.prototype.invalidText = "El valor en este campo es inválido";
 }
 
 Date.monthNames = [
@@ -40,17 +40,17 @@ Date.dayNames = [
    "Domingo",
    "Lunes",
    "Martes",
-   "Miercoles",
+   "Miércoles",
    "Jueves",
    "Viernes",
-   "Sabado"
+   "Sábado"
 ];
 
 if(Ext.MessageBox){
    Ext.MessageBox.buttonText = {
-      ok     : "OK",
+      ok     : "Aceptar",
       cancel : "Cancelar",
-      yes    : "Si",
+      yes    : "Sí",
       no     : "No"
    };
 }
@@ -66,8 +66,8 @@ if(Ext.util.Format){
 if(Ext.DatePicker){
    Ext.apply(Ext.DatePicker.prototype, {
       todayText         : "Hoy",
-      minText           : "Esta fecha es antes de la fecha mínima",
-      maxText           : "Esta fecha es despues de la fecha máxima",
+      minText           : "Esta fecha es anterior a la fecha mínima",
+      maxText           : "Esta fecha es posterior a la fecha máxima",
       disabledDaysText  : "",
       disabledDatesText : "",
       monthNames	: Date.monthNames,
@@ -87,9 +87,9 @@ if(Ext.PagingToolbar){
       firstText      : "Primera página",
       prevText       : "Página anterior",
       nextText       : "Página siguiente",
-      lastText       : "Ultima página",
+      lastText       : "Última página",
       refreshText    : "Actualizar",
-      displayMsg     : "Ver {0} - {1} de {2}",
+      displayMsg     : "Mostrando {0} - {1} de {2}",
       emptyMsg       : 'Sin datos para mostrar'
    });
 }
@@ -98,7 +98,7 @@ if(Ext.form.TextField){
    Ext.apply(Ext.form.TextField.prototype, {
       minLengthText : "El tamaño mínimo para este campo es de {0}",
       maxLengthText : "El tamaño máximo para este campo es de {0}",
-      blankText     : "Campo obligatorio",
+      blankText     : "Este campo es obligatorio",
       regexText     : "",
       emptyText     : null
    });
@@ -116,9 +116,9 @@ if(Ext.form.DateField){
    Ext.apply(Ext.form.DateField.prototype, {
       disabledDaysText  : "Deshabilitado",
       disabledDatesText : "Deshabilitado",
-      minText           : "La fecha para este campo debe ser despues de {0}",
+      minText           : "La fecha para este campo debe ser después de {0}",
       maxText           : "La fecha para este campo debe ser antes de {0}",
-      invalidText       : "{0} no es una fecha válida - debe ser en formato {1}",
+      invalidText       : "{0} no es una fecha válida - debe tener el formato {1}",
       format            : "d/m/Y"
    });
 }
@@ -132,17 +132,17 @@ if(Ext.form.ComboBox){
 
 if(Ext.form.VTypes){
    Ext.apply(Ext.form.VTypes, {
-      emailText    : 'Este campo debe ser en formato e-mail "usuario@email.com"',
-      urlText      : 'Este campo debe ser en formato URL "http:/'+'/www.midireccionurl.com"',
-      alphaText    : 'Este campo solo puede contener letras y _',
-      alphanumText : 'Este campo solo puede contener letras, números y _'
+      emailText    : 'Este campo debe ser una dirección de correo con el formato "usuario@dominio.com"',
+      urlText      : 'Este campo debe ser una con el formato "http:/'+'/www.dominio.com"',
+      alphaText    : 'Este campo solo debe contener letras y _',
+      alphanumText : 'Este campo solo debe contener letras, números y _'
    });
 }
 
 if(Ext.grid.GridView){
    Ext.apply(Ext.grid.GridView.prototype, {
-      sortAscText  : "Ordenar Ascendente",
-      sortDescText : "Ordenar Descendente",
+      sortAscText  : "Ordenar en forma ascendente",
+      sortDescText : "Ordenar en forma descendente",
       lockText     : "Bloquear Columna",
       unlockText   : "Desbloquear Columna",
       columnsText  : "Columnas"
