a90c47693a63770461c853b1bf5481ca24c57d1f
jslocum <jslocum@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-24 13:38:40 +0000

    git-svn-id: http://code.extjs.com/svn/ext/trunk@261 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/panels/panels.js
+++ examples/panels/panels.js
@@ -1,3 +1,8 @@
+
+/*
+ WARNING - DO NOT USE THESE CLASSES THEY ARE STILL IN DEV
+ */
+
 Ext.Window = function(config){
     Ext.Window.superclass.constructor.call(this, config);
 
@@ -14,11 +19,20 @@ Ext.extend(Ext.Window, Ext.Panel, {
     resizable:true,
     draggable:true,
     closable : true,
-
+    constrain:true,
+    
     minimizable : true,
     maximizable : true,
 
-    tools:[{id:'minimize'},{id:'maximize'},{id:'restore', hidden:true},{id:'close'}],
+    minHeight: 80,
+    minWidth: 200,
+    minButtonWidth: 75,
+    defaultButton: null,
+    buttonAlign: "right",
+
+    initHidden : true,
+
+    //tools:[{id:'minimize'},{id:'maximize'},{id:'restore', hidden:true},{id:'close'}],
 
     onRender : function(ct){
         Ext.Window.superclass.onRender.call(this, ct);
@@ -28,11 +42,20 @@ Ext.extend(Ext.Window, Ext.Panel, {
                     tag: "a", href:"#", cls:"x-dlg-focus",
                     tabIndex:"-1", onclick:'return false;'});
 
-        this.proxy = this.el.createProxy("x-window-proxy", ct);
+        this.proxy = this.el.createProxy("x-window-proxy");
         this.proxy.enableDisplayMode('block');
+
+        if(this.modal){
+            this.mask = Ext.get(document.body).createChild({cls:"ext-el-mask"});
+            this.mask.enableDisplayMode("block");
+        }
     },
 
     initEvents : function(){
+        if(this.animateTarget){
+            this.setAnimateTarget(this.animateTarget);
+        }
+
         if(this.resizable){
             this.resizer = new Ext.Resizable(this.el, {
                 minWidth: this.minWidth,
@@ -48,6 +71,46 @@ Ext.extend(Ext.Window, Ext.Panel, {
         if(this.draggable){
             this.header.addClass("x-window-draggable");
             this.dd = new Ext.Window.DD(this);
+        }
+        this.initTools();
+
+        this.el.on("mousedown", this.toFront, this);
+        //Ext.EventManager.onWindowResize(this.adjustViewport, this, true);
+        Ext.DialogManager.register(this);
+        this.hidden = true;
+    },
+
+    initTools : function(){
+        if(this.minimizable){
+            this.addTool({
+                id: 'minimize',
+                on: {
+                    'click' : this.hide.createDelegate(this, [])
+                }
+            });
+        }
+        if(this.maximizable){
+            this.addTool({
+                id: 'maximize',
+                on: {
+                    'click' : this.hide.createDelegate(this, [])
+                }
+            });
+            this.addTool({
+                id: 'restore',
+                on: {
+                    'click' : this.hide.createDelegate(this, [])
+                },
+                hidden:true
+            });
+        }
+        if(this.closable){
+            this.addTool({
+                id: 'close',
+                on: {
+                    'click' : this.hide.createDelegate(this, [])
+                }
+            });
         }
     },
 
@@ -68,9 +131,191 @@ Ext.extend(Ext.Window, Ext.Panel, {
         this.updateBox(box);
         this.focus();
         this.fireEvent("resize", this, box.width, box.height);
+    },
+
+    /**
+     * Focuses the dialog.  If a defaultButton is set, it will receive focus, otherwise the
+     * dialog itself will receive focus.
+     */
+    focus : function(){
+        this.focusEl.focus();
+    },
+
+    setAnimateTarget : function(el){
+        el = Ext.get(el);
+        this.animateTarget = el;
+    },
+
+    beforeShow : function(){
+        delete this.el.lastXY;
+        delete this.el.lastLT;
+        this.el.setLeftTop(this.x, this.y);
+        this.expand(false);
+        if(this.modal){
+            Ext.get(document.body).addClass("x-body-masked");
+            this.mask.setSize(Ext.lib.Dom.getViewWidth(true), Ext.lib.Dom.getViewHeight(true));
+            this.mask.show();
+        }
+    },
+
+    show : function(animateTarget, cb, scope){
+        if (this.hidden === false || this.fireEvent("beforeshow", this) === false){
+            return;
+        }
+        if(cb){
+            this.on('show', cb, scope, {single:true});
+        }
+        this.hidden = false;
+        if(animateTarget !== undefined){
+            this.setAnimateTarget(animateTarget);
+        }
+        this.beforeShow();
+        if(this.animateTarget){
+            this.animShow();
+        }else{
+            this.afterShow();
+        }
+    },
+
+    afterShow : function(){
+        this.proxy.hide();
+        this.el.setStyle('display', 'block');
+        this.el.show();
+        this.toFront();
+        this.focus();
+        this.fireEvent("show", this);
+    },
+
+    // private
+    animShow : function(){
+        this.proxy.show();
+        this.proxy.setBox(this.animateTarget.getBox());
+        var b = this.getBox(false);
+        b.callback = this.afterShow;
+        b.scope = this;
+        this.el.setStyle('display', 'none');
+        this.proxy.shift(b);
+    },
+
+
+    hide : function(animateTarget, cb, scope){
+        if(this.hidden || this.fireEvent("beforehide", this) === false){
+            return;
+        }
+        if(cb){
+            this.on('hide', cb, scope, {single:true});
+        }
+        this.hidden = true;
+        if(animateTarget !== undefined){
+            this.setAnimateTarget(animateTarget);
+        }
+        if(this.animateTarget){
+            this.animHide();
+        }else{
+            this.el.hide();
+            this.afterHide();
+        }
+    },
+
+    afterHide : function(){
+        this.proxy.hide();
+        if(this.modal){
+            this.mask.hide();
+            Ext.get(document.body).removeClass("x-body-masked");
+        }
+        this.fireEvent("hide", this);
+    },
+
+    animHide : function(){
+        this.proxy.show();
+        var tb = this.getBox(false);
+        this.proxy.setBox(tb);
+        this.el.hide();
+        var b = this.animateTarget.getBox();
+        b.callback = this.afterHide;
+        b.scope = this;
+        this.proxy.shift(b);
+    },
+
+    // private
+    // z-index is managed by the DialogManager and may be overwritten at any time
+    setZIndex : function(index){
+        if(this.modal){
+            this.mask.setStyle("z-index", index);
+        }
+        this.el.setZIndex(++index);
+        index += 5;
+
+        if(this.resizer){
+            this.resizer.proxy.setStyle("z-index", ++index);
+        }
+
+        this.lastZIndex = index;
+    },
+
+    /**
+     * Aligns the window to the specified element
+     * @param {String/HTMLElement/Ext.Element} element The element to align to.
+     * @param {String} position The position to align to (see {@link Ext.Element#alignTo} for more details).
+     * @param {Array} offsets (optional) Offset the positioning by [x, y]
+     * @return {Ext.BasicDialog} this
+     */
+    alignTo : function(element, position, offsets){
+        var xy = this.el.getAlignToXY(element, position, offsets);
+        this.setPagePosition(xy[0], xy[1]);
+        return this;
+    },
+
+    /**
+     * Anchors this window to another element and realigns it when the window is resized or scrolled.
+     * @param {String/HTMLElement/Ext.Element} element The element to align to.
+     * @param {String} position The position to align to (see {@link Ext.Element#alignTo} for more details)
+     * @param {Array} offsets (optional) Offset the positioning by [x, y]
+     * @param {Boolean/Number} monitorScroll (optional) true to monitor body scroll and reposition. If this parameter
+     * is a number, it is used as the buffer delay (defaults to 50ms).
+     * @return {Ext.BasicDialog} this
+     */
+    anchorTo : function(el, alignment, offsets, monitorScroll){
+        var action = function(){
+            this.alignTo(el, alignment, offsets);
+        };
+        Ext.EventManager.onWindowResize(action, this);
+        var tm = typeof monitorScroll;
+        if(tm != 'undefined'){
+            Ext.EventManager.on(window, 'scroll', action, this,
+                {buffer: tm == 'number' ? monitorScroll : 50});
+        }
+        action.call(this);
+        return this;
+    },
+
+    /**
+     * Brings this dialog to the front of any other visible dialogs
+     * @return {Ext.BasicDialog} this
+     */
+    toFront : function(){
+        Ext.DialogManager.bringToFront(this);
+        return this;
+    },
+
+    /**
+     * Sends this dialog to the back (under) of any other visible dialogs
+     * @return {Ext.BasicDialog} this
+     */
+    toBack : function(){
+        Ext.DialogManager.sendToBack(this);
+        return this;
+    },
+
+    /**
+     * Centers this dialog in the viewport
+     * @return {Ext.BasicDialog} this
+     */
+    center : function(){
+        var xy = this.el.getCenterXY(true);
+        this.setPagePosition(xy[0], xy[1]);
+        return this;
     }
-
-
 });
 
 Ext.Window.DD = function(win){
@@ -81,25 +326,28 @@ Ext.Window.DD = function(win){
 };
 
 Ext.extend(Ext.Window.DD, Ext.dd.DD, {
+    moveOnly:true,
     startDrag : function(){
         var w = this.win;
         this.proxy = w.createGhost();
         var box = w.getBox(true);
         this.proxy.setLeftTop(box.x, box.y);
-        this.proxy.setSize(box.width, box.height);
+        this.proxy.setWidth(box.width);
+        w.el.hide();
         if(w.constrain !== false){
-            var so = w.shadowOffset;
+            var so = w.el.shadowOffset;
             this.constrainTo(w.container, {right: so, left: so, bottom: so});
         }
     },
     b4Drag : Ext.emptyFn,
     
     onDrag : function(e){
-        this.alignElWithMouse(this.proxy.dom, e.getPageX(), e.getPageY());
+        this.alignElWithMouse(this.proxy, e.getPageX(), e.getPageY());
     },
 
     endDrag : function(e){
         var w = this.win;
+        w.el.show();
         w.setPosition(this.proxy.getLeft(true), this.proxy.getTop(true));
         this.proxy.remove();
         delete this.proxy;
@@ -126,11 +374,28 @@ Ext.onReady(function(){
     });
 
     p2.render(document.body);
+
+    new Ext.Button(document.body, {
+        text: 'Toggle Window',
+        handler : function(){
+            p2[p2.isVisible() ? 'hide' : 'show'](this.el);
+        }
+    });
+
     // stick toolbar in the top bar
     var tb = new Ext.Toolbar(p2.tbar, [{
         text:'Load Content',
         handler : function(){
             p2.load({url:'../tabs/ajax1.htm'});
+        }
+    }]);
+
+
+    // stick toolbar in the top bar
+    var tb2 = new Ext.Toolbar(p2.bbar, [{
+        text:'Do Something',
+        handler : function(){
+            Ext.Msg.alert('Yeah', 'The buttons works!');
         }
     }]);
 
