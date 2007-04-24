d52e6001ea4012f158cd1098bcfd71464b2fba51
trbs <trbs@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-04-24 22:55:06 +0000

    cleanup map prototype
    git-svn-id: http://code.extjs.com/svn/ext/trunk@265 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/locale/multi-lang.js
+++ examples/locale/multi-lang.js
@@ -1,22 +1,19 @@
 /* Fix for Opera, which does not seem to include the map function on Array's */
-if (!Array.prototype.map)
-{
-  Array.prototype.map = function(fun /*, thisp*/)
-  {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw new TypeError();
-
-    var res = new Array(len);
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in this)
-        res[i] = fun.call(thisp, this[i], i, this);
-    }
-
-    return res;
-  };
+if(!Array.prototype.map){
+    Array.prototype.map = function(fun){
+	var len = this.length;
+	if(typeof fun != "function"){
+	    throw new TypeError();
+	}
+	var res = new Array(len);
+	var thisp = arguments[1];
+	for(var i = 0; i < len; i++){
+	    if(i in this){
+		res[i] = fun.call(thisp, this[i], i, this);
+	    }
+	}
+        return res;
+     };
 }
 
 /* Paging Memory Proxy, allows to use paging grid with in memory dataset */
