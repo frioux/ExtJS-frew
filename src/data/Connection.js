285b424e21a2950800fae346b006f855a2a7c7d8
nwhite <nwhite@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-06-08 19:27:43 +0000

    Added a bit of clarification about responses from file uploads.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@517 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/data/Connection.js
+++ src/data/Connection.js
@@ -6,11 +6,16 @@
  * <p>
  * Requests made by this class are asynchronous, and will return immediately. No data from
  * the server will be available to the statement immediately following the {@link #request} call.
- * To process returned data, use a callback in the request options object, or an event listener.</p>
+ * To process returned data, use a callback in the request options object, or an event listener.</p><br>
  * <p>
  * Note: If you are doing a file upload, you will not get a normal response object sent back to
- * your callback or event handler.  Since the upload is handled via in IFRAME, there is no HTTP Response.
- * The response object in this case will be simply {responseText:'', responseXML:null}.</p>
+ * your callback or event handler.  Since the upload is handled via in IFRAME, there is no XMLHttpRequest.
+ * The response object is created using the innerHTML of the IFRAME's document as the responseText
+ * property and, if present, the IFRAME's XML document as the responseXML property.</p><br>
+ * This means that a valid XML or HTML document must be returned. If JSON data is required, it is suggested
+ * that it be placed either inside a &lt;textarea> in an HTML document and retrieved from the responseText
+ * using a regex, or inside a CDATA section in an XML document and retrieved from the responseXML using
+ * standard DOM methods.
  * @constructor
  * @param {Object} config a configuration object.
  */
