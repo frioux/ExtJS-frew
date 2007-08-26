bf3dfc1a068b31b8b84db257fdf52c21c1c556b6
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-08-26 07:54:03 +0000

    Minor doc edits
    git-svn-id: http://code.extjs.com/svn/ext/trunk@857 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/data/DataReader.js
+++ src/data/DataReader.js
@@ -1,3 +1,5 @@
+// Base class for reading structured data from a data source.  This class is intended to be
+// extended (see ArrayReader, JsonReader and XmlReader) and should not be created directly.
 Ext.data.DataReader = function(meta, recordType){
     this.meta = meta;
     this.recordType = recordType instanceof Array ? 
