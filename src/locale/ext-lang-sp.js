505dd5b989b85f0d3c445c422876323977361896
trbs <trbs@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-09-18 15:49:25 +0000

    updated spanish translation by Rafaga2k
    git-svn-id: http://code.extjs.com/svn/ext/trunk@975 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/locale/ext-lang-sp.js
+++ src/locale/ext-lang-sp.js
@@ -1,6 +1,7 @@
 /*
  * Spanish/Latin American Translation by genius551v 04-08-2007
  * Revised by efege, 2007-04-15.
+ * Revised by Rafaga2k 10-01-2007 (mm/dd/yyyy)
  */
 
 Ext.UpdateManager.defaults.indicatorText = '<div class="loading-indicator">Cargando...</div>';
@@ -14,11 +15,11 @@ if(Ext.grid.Grid){
 }
 
 if(Ext.TabPanelItem){
-   Ext.TabPanelItem.prototype.closeText = "Cerrar esta pestaña";
+   Ext.TabPanelItem.prototype.closeText = "Cerrar esta pesta&#241;a";
 }
 
 if(Ext.form.Field){
-   Ext.form.Field.prototype.invalidText = "El valor en este campo es inválido";
+   Ext.form.Field.prototype.invalidText = "El valor en este campo es inv&#225;lido";
 }
 
 if(Ext.LoadMask){
@@ -44,17 +45,17 @@ Date.dayNames = [
    "Domingo",
    "Lunes",
    "Martes",
-   "Miércoles",
+   "Mi&#233;rcoles",
    "Jueves",
    "Viernes",
-   "Sábado"
+   "S&#225;bado"
 ];
 
 if(Ext.MessageBox){
    Ext.MessageBox.buttonText = {
       ok     : "Aceptar",
       cancel : "Cancelar",
-      yes    : "Sí",
+      yes    : "S&#237;",
       no     : "No"
    };
 }
@@ -70,15 +71,15 @@ if(Ext.util.Format){
 if(Ext.DatePicker){
    Ext.apply(Ext.DatePicker.prototype, {
       todayText         : "Hoy",
-      minText           : "Esta fecha es anterior a la fecha mínima",
-      maxText           : "Esta fecha es posterior a la fecha máxima",
+      minText           : "Esta fecha es anterior a la fecha m&#237;nima",
+      maxText           : "Esta fecha es posterior a la fecha m&#225;xima",
       disabledDaysText  : "",
       disabledDatesText : "",
       monthNames	: Date.monthNames,
       dayNames		: Date.dayNames,
       nextText          : 'Mes Siguiente (Control+Right)',
       prevText          : 'Mes Anterior (Control+Left)',
-      monthYearText     : 'Seleccione un mes (Control+Up/Down para desplazar el año)',
+      monthYearText     : 'Seleccione un mes (Control+Up/Down para desplazar el a&#241;o)',
       todayTip          : "{0} (Barra espaciadora)",
       format            : "d/m/Y"
    });
@@ -86,12 +87,12 @@ if(Ext.DatePicker){
 
 if(Ext.PagingToolbar){
    Ext.apply(Ext.PagingToolbar.prototype, {
-      beforePageText : "Página",
+      beforePageText : "P&#225;gina",
       afterPageText  : "de {0}",
-      firstText      : "Primera página",
-      prevText       : "Página anterior",
-      nextText       : "Página siguiente",
-      lastText       : "Última página",
+      firstText      : "Primera p&#225;gina",
+      prevText       : "P&#225;gina anterior",
+      nextText       : "P&#225;gina siguiente",
+      lastText       : "Última p&#225;gina",
       refreshText    : "Actualizar",
       displayMsg     : "Mostrando {0} - {1} de {2}",
       emptyMsg       : 'Sin datos para mostrar'
@@ -100,8 +101,8 @@ if(Ext.PagingToolbar){
 
 if(Ext.form.TextField){
    Ext.apply(Ext.form.TextField.prototype, {
-      minLengthText : "El tamaño mínimo para este campo es de {0}",
-      maxLengthText : "El tamaño máximo para este campo es de {0}",
+      minLengthText : "El tama&#241;o m&#237;nimo para este campo es de {0}",
+      maxLengthText : "El tama&#241;o m&#225;ximo para este campo es de {0}",
       blankText     : "Este campo es obligatorio",
       regexText     : "",
       emptyText     : null
@@ -110,9 +111,9 @@ if(Ext.form.TextField){
 
 if(Ext.form.NumberField){
    Ext.apply(Ext.form.NumberField.prototype, {
-      minText : "El valor mínimo para este campo es de {0}",
-      maxText : "El valor máximo para este campo es de {0}",
-      nanText : "{0} no es un número válido"
+      minText : "El valor m&#237;nimo para este campo es de {0}",
+      maxText : "El valor m&#225;ximo para este campo es de {0}",
+      nanText : "{0} no es un n&#250;mero v&#225;lido"
    });
 }
 
@@ -122,7 +123,7 @@ if(Ext.form.DateField){
       disabledDatesText : "Deshabilitado",
       minText           : "La fecha para este campo debe ser posterior a {0}",
       maxText           : "La fecha para este campo debe ser anterior a {0}",
-      invalidText       : "{0} no es una fecha válida - debe tener el formato {1}",
+      invalidText       : "{0} no es una fecha v&#225;lida - debe tener el formato {1}",
       format            : "d/m/Y"
    });
 }
@@ -136,10 +137,10 @@ if(Ext.form.ComboBox){
 
 if(Ext.form.VTypes){
    Ext.apply(Ext.form.VTypes, {
-      emailText    : 'Este campo debe ser una dirección de correo electrónico con el formato "usuario@dominio.com"',
+      emailText    : 'Este campo debe ser una direcci&#243;n de correo electr&#243;nico con el formato "usuario@dominio.com"',
       urlText      : 'Este campo debe ser una URL con el formato "http:/'+'/www.dominio.com"',
       alphaText    : 'Este campo solo debe contener letras y _',
-      alphanumText : 'Este campo solo debe contener letras, números y _'
+      alphanumText : 'Este campo solo debe contener letras, n&#250;meros y _'
    });
 }
 
@@ -165,5 +166,83 @@ if(Ext.SplitLayoutRegion){
    Ext.apply(Ext.SplitLayoutRegion.prototype, {
       splitTip            : "Arrastre para redimensionar.",
       collapsibleSplitTip : "Arrastre para redimensionar. Doble clic para ocultar."
+   });
+}
+
+if(Ext.form.HtmlEditor){
+   Ext.apply(Ext.form.HtmlEditor.prototype, {
+      createLinkText : "Por favor proporcione el URL para el enlace:",
+          buttonTips : {
+              bold : {
+                  title: 'Negritas (Ctrl+B)',
+                  text: 'Transforma el texto seleccionado en Negritas.',
+                  cls: 'x-html-editor-tip'
+              },
+              italic : {
+                  title: 'Italica (Ctrl+I)',
+                  text: 'Transforma el texto seleccionado en Italicas.',
+                  cls: 'x-html-editor-tip'
+              },
+              underline : {
+                  title: 'Subrayado (Ctrl+U)',
+                  text: 'Subraya el texto seleccionado.',
+                  cls: 'x-html-editor-tip'
+              },
+              increasefontsize : {
+                  title: 'Aumentar la fuente',
+                  text: 'Aumenta el tama&#241;o de la fuente',
+                  cls: 'x-html-editor-tip'
+              },
+              decreasefontsize : {
+                  title: 'Reducir la fuente',
+                  text: 'Reduce el tama&#241;o de la fuente.',
+                  cls: 'x-html-editor-tip'
+              },
+              backcolor : {
+                  title: 'Color de fondo',
+                  text: 'Modifica el color de fondo del texto seleccionado.',
+                  cls: 'x-html-editor-tip'
+              },
+              forecolor : {
+                  title: 'Color de la fuente',
+                  text: 'Modifica el color del texto seleccionado.',
+                  cls: 'x-html-editor-tip'
+              },
+              justifyleft : {
+                  title: 'Alinear a la izquierda',
+                  text: 'Alinea el texto a la izquierda.',
+                  cls: 'x-html-editor-tip'
+              },
+              justifycenter : {
+                  title: 'Centrar',
+                  text: 'Centrar el texto.',
+                  cls: 'x-html-editor-tip'
+              },
+              justifyright : {
+                  title: 'Alinear a la derecha',
+                  text: 'Alinea el texto a la derecha.',
+                  cls: 'x-html-editor-tip'
+              },
+              insertunorderedlist : {
+                  title: 'Lista de vi&#241;etas',
+                  text: 'Inicia una lista con vi&#241;etas.',
+                  cls: 'x-html-editor-tip'
+              },
+              insertorderedlist : {
+                  title: 'Lista numerada',
+                  text: 'Inicia una lista numerada.',
+                  cls: 'x-html-editor-tip'
+              },
+              createlink : {
+                  title: 'Enlace',
+                  text: 'Inserta un enlace de hipertexto.',
+                  cls: 'x-html-editor-tip'
+              },
+              sourceedit : {
+                  title: 'Codigo Fuente',
+                  text: 'Pasar al modo de edicion de codigo fuente.',
+                  cls: 'x-html-editor-tip'
+              }
+        }
    });
 }
