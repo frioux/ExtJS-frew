779202fe7f1388d43d7f9b875947e0dfb592b39a
mystix <mystix@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2007-10-22 11:29:07 +0000

    minor doc fix
    git-svn-id: http://code.extjs.com/svn/ext/trunk@1253 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/core/Element.js
+++ src/core/Element.js
@@ -1865,7 +1865,7 @@ el.alignTo("other-el", "c-bl", [-6, 0]);
     * Update the innerHTML of this element, optionally searching for and processing scripts
     * @param {String} html The new HTML
     * @param {Boolean} loadScripts (optional) true to look for and process scripts
-    * @param {Function} callback For async script loading you can be noticed when the update completes
+    * @param {Function} callback For async script loading you can be notified when the update completes
     * @return {Ext.Element} this
      */
     update : function(html, loadScripts, callback){
