dff0ee0aa99dc4407d11fd9bd01e1fa094811a65
nwhite <nwhite@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2008-03-26 12:24:54 +0000

    Fix markup in doc comments. Attempt to make EVEN CLEARER to people that Ext.get does not return a Component!
    git-svn-id: http://code.extjs.com/svn/ext/trunk@1815 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/core/Element.js
+++ src/core/Element.js
@@ -1121,7 +1121,8 @@ El.prototype = {
      * Appends an event handler to this element.  The shorthand version {@link #on} is equivalent.
      * @param {String} eventName The type of event to handle
      * @param {Function} fn The handler function the event invokes
-     * @param {Object} scope (optional) The scope (this element) of the handler function
+     * @param {Object} scope (optional) The scope (The <tt>this</tt> reference) of the handler function. Defaults
+     * to this Element.
      * @param {Object} options (optional) An object containing handler configuration properties.
      * This may contain any of the following properties:<ul>
      * <li>scope {Object} : The scope in which to execute the handler function. The handler function's "this" context.</li>
@@ -1149,11 +1150,11 @@ el.on('click', this.onClick, this, {
     delay: 100,
     stopEvent : true,
     forumId: 4
-});</code></pre>
+});</code></pre></p>
      * <p>
      * <b>Attaching multiple handlers in 1 call</b><br>
       * The method also allows for a single argument to be passed which is a config object containing properties
-     * which specify multiple handlers.
+     * which specify multiple handlers.</p>
      * <p>
      * Code:<pre><code>
 el.on({
@@ -1170,7 +1171,7 @@ el.on({
         fn: this.onMouseOut,
         scope: this
     }
-});</code></pre>
+});</code></pre></p>
      * <p>
      * Or a shorthand syntax:<br>
      * Code:<pre><code>
@@ -1179,7 +1180,7 @@ el.on({
     'mouseover' : this.onMouseOver,
     'mouseout' : this.onMouseOut,
     scope: this
-});</code></pre>
+});</code></pre></p>
      */
     addListener : function(eventName, fn, scope, options){
         Ext.EventManager.on(this.dom,  eventName, fn, scope || this, options);
@@ -2859,10 +2860,14 @@ El.cache = {};
 var docEl;
 
 /**
- * Static method to retrieve Element objects. Uses simple caching to consistently return the same object.
- * Automatically fixes if an object was recreated with the same id via AJAX or DOM.
+ * Static method to retrieve Ext.Element objects.
+ * <p><b>This method does not retrieve {@link Ext.Component Component}s.</b> This method
+ * retrieves Ext.Element objects which encapsulate DOM elements. To retrieve a Component by
+ * its ID, use {@link Ext.ComponentMgr#get}.</p>
+ * <p>Uses simple caching to consistently return the same object.
+ * Automatically fixes if an object was recreated with the same id via AJAX or DOM.</p>
  * @param {Mixed} el The id of the node, a DOM Node or an existing Element.
- * @return {Element} The Element object (or null if no matching element was found)
+ * @return {Element} The {@link Ext.Element Element} object (or null if no matching element was found)
  * @static
  */
 El.get = function(el){
