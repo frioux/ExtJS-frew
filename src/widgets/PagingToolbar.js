824d440a9fef0598e47482906f074f1899a58cde
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2008-04-04 20:26:57 +0000

    Clarified the desc of afterPageText
    git-svn-id: http://code.extjs.com/svn/ext/trunk@1916 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/PagingToolbar.js
+++ src/widgets/PagingToolbar.js
@@ -46,7 +46,9 @@ Ext.PagingToolbar = Ext.extend(Ext.Toolbar, {
      */
     beforePageText : "Page",
     /**
-     * Customizable piece of the default paging text (defaults to "of {0}")
+     * Customizable piece of the default paging text (defaults to "of {0}"). Note that this string is
+     * formatted using {0} as a token that is replaced by the number of total pages. This token should be 
+     * preserved when overriding this string if showing the total page count is desired.
      * @type String
      */
     afterPageText : "of {0}",
