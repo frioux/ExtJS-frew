0cf52faf7cd05329334ecee794f0727e59b6ac70
jslocum <jslocum@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2008-04-08 01:00:57 +0000

    Add id to <label> element
    git-svn-id: http://code.extjs.com/svn/ext/trunk@1931 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/form/Label.js
+++ src/widgets/form/Label.js
@@ -19,6 +19,7 @@ Ext.form.Label = Ext.extend(Ext.BoxComponent, {
     onRender : function(ct, position){
         if(!this.el){
             this.el = document.createElement('label');
+            this.el.id = this.getId();
             this.el.innerHTML = this.text ? Ext.util.Format.htmlEncode(this.text) : (this.html || '');
             if(this.forId){
                 this.el.setAttribute('htmlFor', this.forId);
