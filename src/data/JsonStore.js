6c16af74045679040228264a8c452bb9c0324d25
mystix <mystix@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2008-04-29 13:16:02 +0000

    minor docfix
    git-svn-id: http://code.extjs.com/svn/ext/trunk@2102 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/data/JsonStore.js
+++ src/data/JsonStore.js
@@ -1,7 +1,7 @@
 /**
  * @class Ext.data.JsonStore
  * @extends Ext.data.Store
- * Small helper class to make creating Stores for remotely-loaded JSON data easier. JsonStore is pre-configured 
+ * Small helper class to make creating Stores for remotely-loaded JSON data easier. JsonStore is pre-configured
  * with a built-in {@link Ext.data.HttpProxy} and {@link Ext.data.JsonReader}.  If you require some other proxy/reader
  * combination then you'll have to create a basic {@link Ext.data.Store} configured as needed.<br/>
 <pre><code>
@@ -33,6 +33,12 @@ var store = new Ext.data.JsonStore({
  * @param {Object} config
  */
 Ext.data.JsonStore = function(c){
+    /**
+     * @cfg {Ext.data.DataReader} reader @hide
+     */
+    /**
+     * @cfg {Ext.data.DataProxy} proxy @hide
+     */
     Ext.data.JsonStore.superclass.constructor.call(this, Ext.apply(c, {
         proxy: !c.data ? new Ext.data.HttpProxy({url: c.url}) : undefined,
         reader: new Ext.data.JsonReader(c, c.fields)
