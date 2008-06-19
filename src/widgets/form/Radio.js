4a4ad8509f14886fec15e11b7e6423bd5d638b26
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2008-06-19 15:59:30 +0000

    Styled checkbox/radio controls and check/radio groups
    git-svn-id: http://code.extjs.com/svn/ext/trunk@2334 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/form/Radio.js
+++ src/widgets/form/Radio.js
@@ -8,8 +8,55 @@
  * @param {Object} config Configuration options
  */
 Ext.form.Radio = Ext.extend(Ext.form.Checkbox, {
+    // private
     inputType: 'radio',
+    // private
+    baseCls: 'x-form-radio',
+    
+    /**
+     * If this radio is part of a group, it will return the selected value
+     * @return {String}
+     */
+    getGroupValue : function(){
+        var c = this.getParent().child('input[name='+this.el.dom.name+']:checked', true);
+        return c ? c.value : null;
+    },
+    
+    // private
+    getParent : function(){
+        return this.el.up('form') || Ext.getBody();
+    },
 
+    // private
+    toggleValue : function() {
+        if(!this.checked){
+            var els = this.getParent().select('input[name='+this.el.dom.name+']');
+            els.each(function(el){
+                if(el.dom.id == this.id){
+                    this.setValue(true);
+                }else{
+                    Ext.getCmp(el.dom.id).setValue(false);
+                }
+            }, this);
+        }
+    },
+    
+    /**
+     * Sets either the checked/unchecked status of this Radio, or, if a string value
+     * is passed, checks a sibling Radio of the same name whose value is the value specified.
+     * @param value {String/Boolean} Checked value, or the value of the sibling radio button to check.
+     */
+    setValue : function(v){
+        if(typeof v=='boolean') {
+            Ext.form.Radio.superclass.setValue.call(this, v);
+        }else{
+            var r = this.getParent().child('input[name='+this.el.dom.name+'][value='+v+']', true);
+            if(r && !r.checked){
+                r.toggleValue();
+            };
+        }
+    },
+    
     /**
      * Overridden and disabled. The editor element does not support standard valid/invalid marking. @hide
      * @method
@@ -19,70 +66,7 @@ Ext.form.Radio = Ext.extend(Ext.form.Checkbox, {
      * Overridden and disabled. The editor element does not support standard valid/invalid marking. @hide
      * @method
      */
-    clearInvalid : Ext.emptyFn,
-
-    valueAsGroup : false,
-
-    /**
-     * If this radio is part of a group, it will return the selected value
-     * @return {String}
-     */
-    getGroupValue : function(){
-    	var p = this.el.up('form') || Ext.getBody();
-        var c = p.child('input[name='+this.el.dom.name+']:checked', true);
-        return c ? c.value : null;
-    },
+    clearInvalid : Ext.emptyFn
     
-    // private
-    onClick : function(){
-    	if(this.el.dom.checked != this.checked){
-    		var p = this.el.up('form') || Ext.getBody();
-			var els = p.select('input[name='+this.el.dom.name+']');
-			els.each(function(el){
-				if(el.dom.id == this.id){
-					this.setValue(true);
-				}else{
-					Ext.getCmp(el.dom.id).setValue(false);
-				}
-			}, this);
-		}
-    },
-
-    /**
-     * Sets either the checked/unchecked status of this Radio, or, if a string value
-     * is passed, checks a sibling Radio of the same name whose value is the value specified.
-     * @param value {String/Boolean} Checked value, or the value of the sibling radio button to check.
-     */
-    setValue : function(v){
-    	if (typeof v == 'boolean') {
-            Ext.form.Radio.superclass.setValue.call(this, v);
-        } else {
-            var rs = this.getRadioElements();
-            for(var i = 0, len = rs.length; i < len; i++){
-                if(rs[i].value == v){
-                    rs[i].checked = true;
-                    rs[i].defaultChecked = true;
-                }
-            }
-        }
-    },
-
-    getValue : function(){
-        if(!this.valueAsGroup) {
-            return Ext.form.Radio.superclass.getValue.call(this);
-        } else {
-            var rs = this.getRadioElements();
-            for(var i = 0, len = rs.length; i < len; i++){
-                if(rs[i].checked){
-                    return rs[i].value;
-                }
-            }
-            return null;
-        }
-    },
-
-    getRadioElements : function(){
-        return this.el.up('form').query('input[name='+this.el.dom.name+']');
-    }
 });
 Ext.reg('radio', Ext.form.Radio);
