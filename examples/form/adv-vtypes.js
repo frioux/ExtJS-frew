09ed7b756d471394b7b0462e9d258dc1b2d2835e
bmoeskau <bmoeskau@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2008-07-16 07:33:39 +0000

    Improved date range validation, fixed freezing issue when monitorValid = true
    git-svn-id: http://code.extjs.com/svn/ext/trunk@2436 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/form/adv-vtypes.js
+++ examples/form/adv-vtypes.js
@@ -1,30 +1,39 @@
 // Add the additional 'advanced' VTypes
 Ext.apply(Ext.form.VTypes, {
-  daterange: function(val, field) {
-    var date = field.parseDate(val);
-    
-    if (field.startDateField) {
-      Ext.getCmp(field.startDateField).setMaxValue(date);
-    } 
-    else if (field.endDateField) {
-      Ext.getCmp(field.endDateField).setMinValue(date);
-    }
-    /* Always return true since we're only using this vtype
-     * to set the min/max allowed values (these are tested
-     * for after the vtype test)
-     */
-    return true;
-  },
-  
-  password: function(val, field) {
-    if (field.initialPassField) {
-      var pwd = Ext.getCmp(field.initialPassField);
-      return (val == pwd.getValue());
-    }
-    return true;
-  },
-  
-  passwordText: 'Passwords do not match'
+    daterange : function(val, field) {
+        var date = field.parseDate(val);
+
+        if(!date){
+            return;
+        }
+        if (field.startDateField && (!this.dateRangeMax || (date.getTime() != this.dateRangeMax.getTime()))) {
+            var start = Ext.getCmp(field.startDateField);
+            start.setMaxValue(date);
+            start.validate();
+            this.dateRangeMax = date;
+        } 
+        else if (field.endDateField && (!this.dateRangeMin || (date.getTime() != this.dateRangeMin.getTime()))) {
+            var end = Ext.getCmp(field.endDateField);
+            end.setMinValue(date);
+            end.validate();
+            this.dateRangeMin = date;
+        }
+        /*
+         * Always return true since we're only using this vtype to set the
+         * min/max allowed values (these are tested for after the vtype test)
+         */
+        return true;
+    },
+
+    password : function(val, field) {
+        if (field.initialPassField) {
+            var pwd = Ext.getCmp(field.initialPassField);
+            return (val == pwd.getValue());
+        }
+        return true;
+    },
+
+    passwordText : 'Passwords do not match'
 });
 
 
