da0e2184c532e6f979eca13a1ad5451699022299
jslocum <jslocum@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2008-10-02 02:09:39 +0000

    Removal of unnecessary variable form of dynamic function
    git-svn-id: http://code.extjs.com/svn/ext/trunk@2657 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/core/DomQuery.js
+++ src/core/DomQuery.js
@@ -379,7 +379,7 @@ Ext.DomQuery = function(){
         compile : function(path, type){
             type = type || "select";
 
-            var fn = ["var f = function(root){\n var mode; ++batch; var n = root || document;\n"];
+            var fn = ["(function(root){\n var mode; ++batch; var n = root || document;\n"];
             var q = path, mode, lq;
             var tk = Ext.DomQuery.matchers;
             var tklen = tk.length;
@@ -444,9 +444,8 @@ Ext.DomQuery = function(){
                     q = q.replace(mm[1], "");
                 }
             }
-            fn[fn.length] = "return nodup(n);\n}";
-            eval(fn.join(""));
-            return f;
+            fn[fn.length] = "return nodup(n);\n})";
+            return eval(fn.join(""));
         },
 
         /**
