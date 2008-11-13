f55b67a48e7d0e5e0db2761b7bd808570f9f6583
jslocum <jslocum@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2008-11-13 20:06:07 +0000

    git-svn-id: http://code.extjs.com/svn/ext/trunk@2740 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

---
+++ examples/direct/php/config.php
@@ -0,0 +1,14 @@
+<?
+$API = array(
+	'TestAction'=>array(
+	    'methods'=>array(
+	        'doEcho'=>array(
+	            'len'=>1
+	        ),
+	        'multiply'=>array(
+	            'len'=>1
+	        )
+	    )
+	)
+);
+?>
