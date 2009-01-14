76c6d80058cbe937ef9e1329419fffa03fee6a02
evant <evant@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-01-14 13:44:55 +0000

    Doc fix, documented defaults for extraCls.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@2907 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/layout/AbsoluteLayout.js
+++ src/widgets/layout/AbsoluteLayout.js
@@ -5,7 +5,12 @@
  * standard x and y component config options.</p>
  */
 Ext.layout.AbsoluteLayout = Ext.extend(Ext.layout.AnchorLayout, {
-    extraCls: 'x-abs-layout-item',
+    /**
+     * @cfg {String} extraCls
+     * An optional extra CSS class that will be added to the container (defaults to 'x-abs-layout-item').  This can be useful for
+     * adding customized styles to the container or any of its children using standard CSS rules.
+     */
+    extraCls: '',
     isForm: false,
     // private
     setContainer : function(ct){
