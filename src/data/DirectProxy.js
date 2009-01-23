4ca8af7ae01524b1e10c2d2bd84b81f6409569a7
aconran <aconran@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-01-23 20:44:54 +0000

    Documentation for DirectProxy and DirectStore
    git-svn-id: http://code.extjs.com/svn/ext/trunk@2962 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/data/DirectProxy.js
+++ src/data/DirectProxy.js
@@ -1,5 +1,9 @@
+/**
+ * @class Ext.data.DirectProxy
+ * @extends Ext.data.DataProxy
+ */
 Ext.data.DirectProxy = function(config){
-	Ext.apply(this, config);
+    Ext.apply(this, config);
     if(typeof this.paramOrder == 'string'){
         this.paramOrder = this.paramOrder.split(/[\s,|]/);
     }
@@ -7,12 +11,24 @@ Ext.data.DirectProxy = function(config){
 };
 
 Ext.extend(Ext.data.DirectProxy, Ext.data.DataProxy, {
-
+	/**
+	 * @cfg paramOrder {Array/String} An array of params and the order which they must be executed on the server-side. Can also be specified as a comma or pipe delimited list. Examples ['arg1','arg2'] or 'arg1|arg2' or 'arg1,arg2'
+	 */
 	paramOrder: undefined,
+	
+	/**
+	 * @cfg paramsAsHash {Boolean}
+	 * Send parameters as a collection of named arguments. Defaults to true. Providing a paramOrder immediately overrides this configuration.
+	 */	
 	paramsAsHash: true,
+	
+	/**
+	 * @cfg directFn {Function}
+	 * The Ext.Direct function which has been imported from the server-side
+	 */	
 	directFn: undefined,
 
-    load: function(params, reader, cb, scope, arg){
+	load: function(params, reader, cb, scope, arg){
 		if(this.fireEvent("beforeload", this, params) !== false) {
 			var args = [];
 			if(this.paramOrder){
@@ -26,21 +42,20 @@ Ext.extend(Ext.data.DirectProxy, Ext.data.DataProxy, {
 				callback: function(result, e){
 					if(!e.status){
 						this.fireEvent("loadexception", this, e, result);
-            			cb.call(scope, null, arg, false);
-            			return;
+						cb.call(scope, null, arg, false);
+		            			return;
 					}
 					var rs;
-			        try {
-			            rs = reader.readRecords(result);
-			        }catch(ex){
-			            this.fireEvent("loadexception", this, e, result, ex);
-			            cb.call(scope, null, arg, false);
-			            return;
-			        }
-			        this.fireEvent("load", this, e, arg);
-			        cb.call(scope, rs, arg, true);
+					try {
+					    rs = reader.readRecords(result);
+					}catch(ex){
+					    this.fireEvent("loadexception", this, e, result, ex);
+					    cb.call(scope, null, arg, false);
+					    return;
+					}
+					this.fireEvent("load", this, e, arg);
+					cb.call(scope, rs, arg, true);
 				},
-
 				scope: this
 			});
 
--- src/data/DirectStore.js
+++ src/data/DirectStore.js
@@ -1,8 +1,28 @@
+/**
+ * @class Ext.data.DirectStore
+ * @extends Ext.data.Store
+ * A pre-configured convenience class of Ext.data.Store which makes interacting
+ * with an Ext.Direct Server-side Provider easier.
+ *
+ * Makes the assumption that you will always use an Ext.data.DirectProxy and an
+ * Ext.data.JsonReader.
+ */
+/**
+ * @cfg paramOrder {Array/String} An array of params and the order which they must be executed on the server-side. Can also be specified as a comma or pipe delimited list. Examples ['arg1','arg2'] or 'arg1|arg2' or 'arg1,arg2'
+ */
+/**
+ * @cfg paramsAsHash {Boolean}
+ * Send parameters as a collection of named arguments. Defaults to true. Providing a paramOrder immediately overrides this configuration.
+ */
+/**
+ * @cfg directFn {Function}
+ * The Ext.Direct function which has been imported from the server-side
+ */
 Ext.data.DirectStore = function(c){
 	var pcfg = Ext.copyTo({}, c, 'paramOrder,paramsAsHash,directFn');
 	Ext.data.DirectStore.superclass.constructor.call(this, Ext.apply(c, {
-        proxy: new Ext.data.DirectProxy(pcfg),
-        reader: new Ext.data.JsonReader(c, c.fields)
-    }));
+		proxy: new Ext.data.DirectProxy(pcfg),
+		reader: new Ext.data.JsonReader(c, c.fields)
+	}));
 };
 Ext.extend(Ext.data.DirectStore, Ext.data.Store);
