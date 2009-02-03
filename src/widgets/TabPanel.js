6010f91906fe550b4bdda1915f9ff74dd5e9cb46
nwhite <nwhite@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-02-03 09:20:34 +0000

    Document some of the causes of problems when using TabPanels.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@3021 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/TabPanel.js
+++ src/widgets/TabPanel.js
@@ -2,10 +2,22 @@
  * @class Ext.TabPanel
  * <p>A basic tab container. TabPanels can be used exactly like a standard {@link Ext.Panel} for layout
  * purposes, but also have special support for containing child Components that are managed using a CardLayout
- * layout manager, and displayed as seperate tabs.</p>
+ * layout manager, and displayed as seperate tabs.
+ * </p>
+ * <p><b>Note:</b> TabPanels use their {@link Ext.Panel#header header} or {@link Ext.Panel#footer footer} element
+ * (depending on the {@link #tabPosition} configuration) to accommodate the tab selector buttons. This means that a TabPanel
+ * will not display any configured title, and will not display any configured header {@link Ext.Panel#tools tools}.
+ * To display a header, embed the TabPanel in a {@link Ext.Container Container} which uses
+ * <b><tt>{@link Ext.Container#layout layout: 'fit'}</tt></b>.
+ * </p>
+ * <p><b>Note:</b> It is advisable to configure all child items of a TabPanel (and any Container which uses a CardLayout) with
+ * <b><tt>{@link Ext.Component#hideMode hideMode: 'offsets'}</tt></b> to avoid rendering errors in child components hidden
+ * using the CSS <tt>display</tt> style.
+ * </p>
  * <p>There is no actual tab class &mdash; each tab is simply an {@link Ext.BoxComponent Component} such
  * as a {@link Ext.Panel Panel}. However, when rendered in a TabPanel, each child Component can fire
- * additional events that only exist for tabs and are not available from other Component. These are:</p>
+ * additional events that only exist for tabs and are not available from other Component. These are:
+ * </p>
  * <ul>
  * <li><b>activate</b>: Fires when this Component becomes the active tab.
  * <div class="mdetail-params">
