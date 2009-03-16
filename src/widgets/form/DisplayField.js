08a6104f51637e3f4940cf0dad0bb4c8ed8e8198
mjlecomte <mjlecomte@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-03-16 23:42:42 +0000

    Doc fix. Start of docs for DisplayField.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@3343 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/form/DisplayField.js
+++ src/widgets/form/DisplayField.js
@@ -1,15 +1,24 @@
 /**
  * @class Ext.form.DisplayField
  * @extends Ext.form.Field
+ * Text field that bypasses validation.
  * @constructor
- * Creates a new Field
+ * Creates a new DisplayField.
  * @param {Object} config Configuration options
  */
 Ext.form.DisplayField = Ext.extend(Ext.form.Field,  {
     validationEvent : false,
     validateOnBlur : false,
     defaultAutoCreate : {tag: "div"},
+    /**
+     * @cfg {String} fieldClass The default CSS class for the field (defaults to <tt>"x-form-display-field"</tt>)
+     */
     fieldClass : "x-form-display-field",
+    /**
+     * @cfg {Boolean} htmlEncode <tt>false</tt> to skip HTML-encoding the text when rendering it (defaults to
+     * <tt>false</tt>). This might be useful if you want to include tags in the field's innerHTML rather than
+     * rendering them as string literals per the default logic.
+     */
     htmlEncode: false,
 
     // private
@@ -53,6 +62,18 @@ Ext.form.DisplayField = Ext.extend(Ext.form.Field,  {
         this.setRawValue(v);
         return this;
     }
+    /** 
+     * @cfg {Boolean} validateOnBlur 
+     * @hide
+     */
+    /** 
+     * @cfg {Number} validationDelay 
+     * @hide
+     */
+    /** 
+     * @cfg {String/Boolean} validationEvent 
+     * @hide
+     */
 });
 
 Ext.reg('displayfield', Ext.form.DisplayField);
