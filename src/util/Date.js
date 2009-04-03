c1a6f38218d70ea9af8951d3498b17d8d7e761ae
nwhite <nwhite@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-04-03 07:37:13 +0000

    Document custom date formatting/parsing functions.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@3499 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/util/Date.js
+++ src/util/Date.js
@@ -99,6 +99,8 @@ Date.patterns = {
 var dt = new Date();
 document.write(dt.format(Date.patterns.ShortDate));
 </code></pre>
+ * <p>Developer-written, custom formats may be used by supplying both a formatting and a parsing function
+ * which perform to specialized requirements. The functions are stored in {@link #parseFunctions} and {@link #formatFunctions}.</p>
  */
 
 /*
@@ -145,7 +147,28 @@ Date.formatCodeToRegex = function(character, currentGroup) {
 var $f = Date.formatCodeToRegex;
 
 Ext.apply(Date, {
-    // private
+    /**
+     * @property parseFunctions
+     * @static
+     * @type Object
+     * <p>An object hash in which each property is a date parsing function. The property name is the
+     * format string which that function parses.</p>
+     * <p>This object is automatically populated with date parsing functions as
+     * date formats are requested for Ext standard formatting strings.</p>
+     * <p>Custom parsing functions may be inserted into this object, keyed by a name which from then on
+     * may be used as a format string to {@link #parseDate}.<p>
+     * <p>Example:</p><code><pre>
+Date.parseFunctions['x-date-format'] = myDateParser;
+</pre></code>
+     * <p>A parsing function should return a Date object, and is passed the following parameters:<div class="mdetail-params"><ul>
+     * <li><code>date</code> : String<div class="sub-desc">The date string to parse.</div></li>
+     * <li><code>strict</code> : Boolean<div class="sub-desc">True to validate date strings while parsing
+     * (i.e. prevent javascript Date "rollover") (The default must be false).
+     * Invalid date strings should return null when parsed.</div></li>
+     * </ul></div></p>
+     * <p>To enable Dates to also be <i>formatted</i> according to that format, a corresponding
+     * formatting function must be placed into the {@link #formatFunctions} property.
+     */
     parseFunctions: {
         "M$": function(input, strict) {
             // note: the timezone offset is ignored since the M$ Ajax server sends 
@@ -156,6 +179,25 @@ Ext.apply(Date, {
         }
     },
     parseRegexes: [],
+
+    /**
+     * @property formatFunctions
+     * @static
+     * @type Object
+     * <p>An object hash in which each property is a date formatting function. The property name is the
+     * format string which corresponds to the produced formatted date string.</p>
+     * <p>This object is automatically populated with date formatting functions as
+     * date formats are requested for Ext standard formatting strings.</p>
+     * <p>Custom formatting functions may be inserted into this object, keyed by a name which from then on
+     * may be used as a format string to {@link #format}. Example:</p><code><pre>
+Date.formatFunctions['x-date-format'] = myDateFormatter;
+</pre></code>
+     * <p>A formatting function should return a string repesentation of the passed Date object:<div class="mdetail-params"><ul>
+     * <li><code>date</code> : Date<div class="sub-desc">The Date to format.</div></li>
+     * </ul></div></p>
+     * <p>To enable date strings to also be <i>parsed</i> according to that format, a corresponding
+     * parsing function must be placed into the {@link #parseFunctions} property.
+     */
     formatFunctions: {
         "M$": function() {
             // UTC milliseconds since Unix epoch (M$-AJAX serialized date format (MRSF))
