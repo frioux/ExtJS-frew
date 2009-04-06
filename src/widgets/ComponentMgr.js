16c7c349f7be5b9bed19b6dc62966c49893fe1b5
nwhite <nwhite@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-04-06 14:12:34 +0000

    Correct doc format for Ext.ComponentMgr.create.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@3537 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/ComponentMgr.js
+++ src/widgets/ComponentMgr.js
@@ -85,8 +85,8 @@ Ext.ComponentMgr = function(){
         /**
          * Creates a new Component from the specified config object using the
          * config object's {@link Ext.component#xtype xtype} to determine the class to instantiate.
-         * @param config {Object} A configuration object for the Component you wish to create.
-         * @param defaultType {Constructor} The constructor to provide the default Component type if
+         * @param {Object} config A configuration object for the Component you wish to create.
+         * @param {Constructor} defaultType The constructor to provide the default Component type if
          * the config object does not contain an xtype. (Optional if the config contains an xtype).
          * @return {Ext.Component} The newly instantiated Component.
          */
