cba71c46593c94ad78cc712353d6a6c0a58e6c18
chrisscott <chrisscott@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-04-06 21:24:08 +0000

    Add @cfg normal examples to panels.html
    git-svn-id: http://code.extjs.com/svn/ext/trunk@3563 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/panel/panels.js
+++ examples/panel/panels.js
@@ -6,4 +6,44 @@ Ext.onReady(function(){
         width:400,
         html: Ext.example.bogusMarkup
     });
+
+	// normal: true
+	new Ext.Panel({
+		title: 'A Panel with W3C-standard body-html styling',
+		normal: true,
+		renderTo: 'container-normal-true',
+		width: 400,
+		html: html.join('')
+	});
+
+	// normal: false
+	new Ext.Panel({
+		title: 'Same panel as above with normal: false',
+		normal: false,
+		renderTo: 'container-normal-false',
+		width: 400,
+		html: html.join('')
+	});
 });
+
+// Some sample html
+var html = [
+	'<h1>Heading One</h1>',
+	'<h2>Heading Two</h2>',
+	'<p>This is a paragraph with <strong>STRONG</strong>, <em>EMPHASIS</em> and a <a href="#">Link</a></p>',
+	'<table border="1">',
+		'<tr>',
+			'<td>Table Column One</td>',
+			'<td>Table Column Two</td>',
+		'</tr>',
+	'</table>',
+	'<ul>',
+		'<li>Un-ordered List-item One</li>',
+		'<li>Un-ordered List-item One</li>',
+	'</ul>',
+	'<ol>',
+		'<li>Ordered List-item One</li>',
+		'<li>Ordered List-item Two</li>',
+	'</ol>',
+	'<blockquote>This is a blockquote</blockquote>'
+];
