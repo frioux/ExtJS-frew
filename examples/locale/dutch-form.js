19b0bf24eda887e9536574942e08879cd6896fe1
tommymaintz <tommymaintz@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-04-07 17:03:59 +0000

    Updated index.html to include Ext Core in menu. Updated locale dutch form sample
    git-svn-id: http://code.extjs.com/svn/ext/trunk@3586 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/locale/dutch-form.js
+++ examples/locale/dutch-form.js
@@ -1,3 +1,79 @@
+Ext.ns('Ext.app');
+Ext.app.DutchForm = Ext.extend(Ext.FormPanel, {
+    locale: {
+        formTitle: 'Contact Informatie (Dutch)',
+        firstName: 'Voornaam',
+        lastName: 'Achternaam',
+        surnamePrefix: 'Tussenvoegsel',
+        company: 'Bedrijf',
+        state: 'Provincie',
+        stateEmptyText: 'Kies een provincie...',
+        email: 'E-mail',
+        birth: 'Geb. Datum',
+        save: 'Opslaan',
+        cancel: 'Annuleren'
+    },
+    
+    initComponent : function(config) {
+        Ext.apply(this, {
+            labelWidth: 100, // label settings here cascade unless overridden
+            url:'save-form.php',
+            frame:true,
+            title: this.locale.formTitle,
+            bodyStyle:'padding:5px 5px 0',
+            width: 350,
+            defaults: {width: 220},
+            defaultType: 'textfield',
+    
+            items: [{
+                    fieldLabel: this.locale.firstName,
+                    name: 'firstname',
+                    allowBlank:false
+                },{
+                    fieldLabel: this.locale.lastName,
+                    name: 'lastName'
+                },{
+                    fieldLabel: this.locale.surnamePrefix,
+                    width: 50,
+                    name: 'surnamePrefix'
+                },{
+                    fieldLabel: this.locale.company,
+                    name: 'company'
+                },  new Ext.form.ComboBox({
+                    fieldLabel: this.locale.province,
+                    hiddenName: 'state',
+                    store: new Ext.data.ArrayStore({
+                        fields: ['provincie'],
+                        data : Ext.exampledata.dutch_provinces // from dutch-provinces.js
+                    }),
+                    displayField: 'provincie',
+                    typeAhead: true,
+                    mode: 'local',
+                    triggerAction: 'all',
+                    emptyText: this.locale.stateEmtyText,
+                    selectOnFocus:true,
+                    width:190
+                }), {
+                    fieldLabel: this.locale.email,
+                    name: 'email',
+                    vtype:'email'
+                }, new Ext.form.DateField({
+                    fieldLabel: this.locale.birth,
+                    name: 'birth'
+                })
+            ],
+    
+            buttons: [{
+                text: this.locale.save
+            },{
+                text: this.locale.cancel
+            }]
+        });
+        
+        Ext.app.DutchForm.superclass.initComponent.apply(this, arguments);
+    }
+});
+
 Ext.onReady(function(){
 
     Ext.QuickTips.init();
@@ -10,60 +86,6 @@ Ext.onReady(function(){
     bd.createChild({tag: 'h2', html: 'Dutch Form'})
     
     // simple form
-    var simple = new Ext.FormPanel({
-        labelWidth: 100, // label settings here cascade unless overridden
-        url:'save-form.php',
-        frame:true,
-        title: 'Contact Informatie (Dutch)',
-        bodyStyle:'padding:5px 5px 0',
-        width: 350,
-        defaults: {width: 220},
-        defaultType: 'textfield',
-
-        items: [{
-                fieldLabel: 'Voornaam',
-                name: 'voornaam',
-                allowBlank:false
-            },{
-                fieldLabel: 'Achternaam',
-                name: 'achternaam'
-            },{
-                fieldLabel: 'Tussenvoegsel',
-                width: 50,
-                name: 'tussenvoegsel'
-            },{
-                fieldLabel: 'Bedrijf',
-                name: 'bedrijf'
-            },  new Ext.form.ComboBox({
-            fieldLabel: 'Provincie',
-            hiddenName: 'state',
-            store: new Ext.data.ArrayStore({
-                fields: ['provincie'],
-                data : Ext.exampledata.dutch_provinces // from dutch-provinces.js
-            }),
-            displayField: 'provincie',
-            typeAhead: true,
-            mode: 'local',
-            triggerAction: 'all',
-            emptyText:'Kies een provincie...',
-            selectOnFocus:true,
-            width:190
-            }), {
-                fieldLabel: 'E-mail',
-                name: 'email',
-                vtype:'email'
-            }, new Ext.form.DateField({
-                fieldLabel: 'Geb. Datum',
-                name: 'geb_datum'
-            })
-        ],
-
-        buttons: [{
-            text: 'Opslaan'
-        },{
-            text: 'Annuleren'
-        }]
-    });
-
+    var simple = new Ext.app.DutchForm();
     simple.render(document.body);
 });
