1bea1abc55e685b71e149d9586be741dc65a7e93
mjlecomte <mjlecomte@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-04-09 05:12:11 +0000

    Docs. PagingToolbar.js  - changed several 'properties' to 'configs'  - clarify that these configs are for quicktips  - add example snippet to illustrate some of the configs  - add more description on why to use the class  - document existing extensions for local paging (FAQ)
    git-svn-id: http://code.extjs.com/svn/ext/trunk@3622 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/PagingToolbar.js
+++ src/widgets/PagingToolbar.js
@@ -1,9 +1,57 @@
 /**
  * @class Ext.PagingToolbar
  * @extends Ext.Toolbar
- * <p>A specialized toolbar that is bound to a {@link Ext.data.Store} and provides automatic paging control. This
- * Component {@link Ext.data.Store#load load}s blocks of data into the Store passing parameters who's names are
- * specified by the store's {@link Ext.data.Store#paramNames paramNames} property.</p>
+ * <p>As the amount of records increases, the time required for the browser to render
+ * them increases. Paging is used to reduce the amount of data exchanged with the client.
+ * Note: if there are more records/rows than can be viewed in the available screen area, vertical
+ * scrollbars will be added.</p>
+ * <p>Paging is typically handled on the server side (see exception below). The client sends
+ * parameters to the server side, which the server needs to interpret and then respond with the
+ * approprate data.</p>
+ * <p><b>Ext.PagingToolbar</b> is a specialized toolbar that is bound to a {@link Ext.data.Store}
+ * and provides automatic paging control. This Component {@link Ext.data.Store#load load}s blocks
+ * of data into the <tt>{@link #store}</tt> by passing {@link #paramNames parameters} used for
+ * paging criteria.</p>
+ * <p>PagingToolbar is typically used as one of the Grid's toolbars:</p>
+ * <pre><code>
+Ext.QuickTips.init(); // to display button quicktips
+
+var myStore = new Ext.data.Store({
+    ... 
+});
+
+var myPageSize = 25;  // server script should only send back 25 items
+
+var grid = new Ext.grid.GridPanel({
+    ...
+    store: myStore,
+    bbar: new Ext.PagingToolbar({
+        store: myStore,       // grid and PagingToolbar using same store
+        displayInfo: true,
+        pageSize: myPageSize,
+        prependButtons: true,
+        items: [
+            'text 1'
+        ]
+    })
+});
+ * </code></pre>
+ * 
+ * <p>To use paging, pass the paging requirements to the server when the store is first loaded.</p> 
+ * <pre><code>
+store.load({
+    params: {
+        start: 0,          // specify params for the first page load if using paging
+        limit: myPageSize,
+        foo:   'bar'
+});
+ * </code></pre>
+ * <p><u>Paging with Local Data</u></p>
+ * <p>Paging can also be accomplished with local data using extensions:</p>
+ * <div class="mdetail-params"><ul>
+ * <li><a href="http://extjs.com/forum/showthread.php?t=57386">Ext.ux.data.PagingStore</a></li>
+ * <li>Paging Memory Proxy (examples/locale/PagingMemoryProxy.js)</li>
+ * </ul></div>
  * @constructor
  * Create a new PagingToolbar
  * @param {Object} config The config object
@@ -15,27 +63,29 @@ var T = Ext.Toolbar;
 
 Ext.PagingToolbar = Ext.extend(Ext.Toolbar, {
     /**
-     * @cfg {Ext.data.Store} store The {@link Ext.data.Store} the paging toolbar should use as its data source (required).
+     * @cfg {Ext.data.Store} store
+     * The {@link Ext.data.Store} the paging toolbar should use as its data source (required).
      */
     /**
      * @cfg {Boolean} displayInfo
-     * True to display the displayMsg (defaults to false)
+     * <tt>true</tt> to display the displayMsg (defaults to <tt>false</tt>)
      */
     /**
      * @cfg {Number} pageSize
-     * The number of records to display per page (defaults to 20)
+     * The number of records to display per page (defaults to <tt>20</tt>)
      */
-    /**
-     * @cfg {Boolean} prependButtons
-     * True to insert any configured items <i>before</i> the paging buttons. Defaults to false.
-     */
-
     pageSize: 20,
     /**
+     * @cfg {Boolean} prependButtons
+     * <tt>true</tt> to insert any configured <tt>items</tt> <i>before</i> the paging buttons.
+     * Defaults to <tt>false</tt>.
+     */
+    /**
      * @cfg {String} displayMsg
-     * The paging status message to display (defaults to "Displaying {0} - {1} of {2}").  Note that this string is
-     * formatted using the braced numbers 0-2 as tokens that are replaced by the values for start, end and total
-     * respectively. These tokens should be preserved when overriding this string if showing those values is desired.
+     * The paging status message to display (defaults to <tt>"Displaying {0} - {1} of {2}"</tt>).
+     * Note that this string is formatted using the braced numbers <tt>{0}-{2}</tt> as tokens
+     * that are replaced by the values for start, end and total respectively. These tokens should
+     * be preserved when overriding this string if showing those values is desired.
      */
     displayMsg : 'Displaying {0} - {1} of {2}',
     /**
@@ -44,45 +94,53 @@ Ext.PagingToolbar = Ext.extend(Ext.Toolbar, {
      */
     emptyMsg : 'No data to display',
     /**
-     * Customizable piece of the default paging text (defaults to "Page")
-     * @type String
+     * @cfg {String} beforePageText
+     * The text displayed before the input item (defaults to <tt>"Page"</tt>).
      */
     beforePageText : "Page",
     /**
-     * Customizable piece of the default paging text (defaults to "of {0}"). Note that this string is
-     * formatted using {0} as a token that is replaced by the number of total pages. This token should be
-     * preserved when overriding this string if showing the total page count is desired.
-     * @type String
+     * @cfg {String} afterPageText
+     * Customizable piece of the default paging text (defaults to <tt>"of {0}"</tt>). Note that
+     * this string is formatted using <tt>{0}</tt> as a token that is replaced by the number of
+     * total pages. This token should be preserved when overriding this string if showing the
+     * total page count is desired.
      */
     afterPageText : "of {0}",
     /**
-     * Customizable piece of the default paging text (defaults to "First Page")
-     * @type String
+     * @cfg {String} firstText
+     * The quicktip text displayed for the first page button (defaults to <tt>"First Page"</tt>).
+     * <b>Note</b>: quick tips must be initialized for the quicktip to show.
      */
     firstText : "First Page",
     /**
-     * Customizable piece of the default paging text (defaults to "Previous Page")
-     * @type String
+     * @cfg {String} prevText
+     * The quicktip text displayed for the previous page button (defaults to <tt>"Previous Page"</tt>).
+     * <b>Note</b>: quick tips must be initialized for the quicktip to show.
      */
     prevText : "Previous Page",
     /**
-     * Customizable piece of the default paging text (defaults to "Next Page")
-     * @type String
+     * @cfg {String} nextText
+     * The quicktip text displayed for the next page button (defaults to <tt>"Next Page"</tt>).
+     * <b>Note</b>: quick tips must be initialized for the quicktip to show.
      */
     nextText : "Next Page",
     /**
-     * Customizable piece of the default paging text (defaults to "Last Page")
-     * @type String
+     * @cfg {String} lastText
+     * The quicktip text displayed for the last page button (defaults to <tt>"Last Page"</tt>).
+     * <b>Note</b>: quick tips must be initialized for the quicktip to show.
      */
     lastText : "Last Page",
     /**
-     * Customizable piece of the default paging text (defaults to "Refresh")
-     * @type String
+     * @cfg {String} lastText
+     * The quicktip text displayed for the Refresh button (defaults to <tt>"Refresh"</tt>).
+     * <b>Note</b>: quick tips must be initialized for the quicktip to show.
      */
     refreshText : "Refresh",
 
     /**
-     * Object mapping of parameter names for load calls (defaults to {start: 'start', limit: 'limit'})
+     * Object mapping of parameter names used for load calls.  This property is affected by
+     * See also {@link Ext.data.Store#paramNames}, but is initially set to:
+     * <pre>{start: 'start', limit: 'limit'}</pre>
      */
     paramNames : {start: 'start', limit: 'limit'},
 
