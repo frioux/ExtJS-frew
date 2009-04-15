187144aa5a7240d7110ea2435805b55add8eb933
chrisscott <chrisscott@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-04-15 19:38:12 +0000

    fixed docs on returnJson @cfg
    git-svn-id: http://code.extjs.com/svn/ext/trunk@3713 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/data/JsonWriter.js
+++ src/data/JsonWriter.js
@@ -5,10 +5,10 @@
  */
 Ext.data.JsonWriter = Ext.extend(Ext.data.DataWriter, {
 	/**
-	 * @cfg {Boolean} returnJson <tt>false</tt> to {@link Ext.util.JSON#encode encode} the
-	 * {@link Ext.data.DataWriter#toHash hashed data}. Defaults to <tt>false</tt>, returning
-	 * the hashed data without encoding.  When using {@link Ext.data.DirectProxy}, set this to <tt>true</tt>
-	 * since Ext.Direct will perform its own json-encoding.
+	 * @cfg {Boolean} returnJson <tt>true</tt> to {@link Ext.util.JSON#encode encode} the
+	 * {@link Ext.data.DataWriter#toHash hashed data}. Defaults to <tt>true</tt>.  When using
+	 * {@link Ext.data.DirectProxy}, set this to <tt>false</tt> since Ext.Direct will perform
+	 * its own json-encoding.
 	 */
 	returnJson : true,
 
