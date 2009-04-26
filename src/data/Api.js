a8b3294e6445d987ef5ae6e81e90dcc0a4713746
chrisscott <chrisscott@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-04-26 01:46:01 +0000

    docs
    git-svn-id: http://code.extjs.com/svn/ext/trunk@3824 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/data/Api.js
+++ src/data/Api.js
@@ -1,6 +1,7 @@
 /**
  * Ext.data.Api is a singleton designed to manage the data API including methods for validating
  * a developer's DataProxy / Ext.Direct API.  Defines CONSTANTS for CRUD-actions create, read, update and destroy.
+ * @singleton
  */
 Ext.data.Api = (function() {
 
@@ -8,26 +9,26 @@ Ext.data.Api = (function() {
         /**
          * @const Ext.data.Api.CREATE Text representing the remote-action "create"
          */
-        CREATE : 'create',
+        CREATE  : 'create',
         /**
-         * @const Ext.data.Api.READ Text representing the remote-action for remotely reading/loading data from server.
-         * It important these names not be changed since they sometimes map to a method on another object, like Ext.data.DataWriter for example.
-         * The name "load" is important for maintaining backwards-compatibility with Ext-2.0, as well.
+         * @const Ext.data.Api.READ Text representing the action for remotely reading/loading data from server.
+         * The word "load" is important for maintaining backwards-compatibility with Ext-2.0, as well.  "read" would be preferred.
+         * However, with the introduction of Ext.data.Api singleton, we my be able to use "read" now.
          */
-        READ     : 'load',
+        READ    : 'load',
         /**
          * @const Ext.data.Api.UPDATE Text representing the remote-action to rupdate records on server.
-         * The word update would be preferred here, instead of "save" but "update" has already been used for events pre-Ext3.
+         * The word "update" would be preferred here, instead of "save" but "update" has already been used for events pre-Ext3.
+         * However, since the introduction of the Ext.data.Api singleton, we may be able to use "udpate" now.
          */
-        UPDATE   : 'save',
+        UPDATE  : 'save',
         /**
-         * @const Ext.data.Api.UPDATE Text representing the remote-action to destroy records on server.
+         * @const Ext.data.Api.DESTROY Text representing the remote-action to destroy records on server.
          */
-        DESTROY  : 'destroy',
+        DESTROY : 'destroy',
 
         /**
          * Returns a list of names of all available CRUD actions
-         * @static
          * @return {String[]}
          */
         getVerbs : function(){
@@ -39,7 +40,6 @@ Ext.data.Api = (function() {
          * @param {String} action
          * @param {String[]}(Optional) List of availabe CRUD actions.  Pass in list when executing multiple times for efficiency.
          * @return {Boolean}
-         * @static
          */
         isVerb : function(action, crud) {
             var found = false;
@@ -56,13 +56,12 @@ Ext.data.Api = (function() {
          * Returns true if the supplied API is valid; that is, that all keys match defined CRUD-actions,
          * Ext.data.CREATE, Ext.data.READ, Ext.data.UPDATE, Ext.data.DESTROY.  Otherwise returns an array of mistakes.
          * @return {String[]||true}
-         * @static
          */
         isValid : function(api){
             var invalid = [];
-            var crud = this.getVerbs();
+            var crud = this.getVerbs(); // <-- cache a copy of teh verbs.
             for (var action in api) {
-                if (!this.isVerb(action, crud)) {
+                if (!this.isVerb(action, crud)) {   // <-- send cache of verbs into isVerb.  This call is only reason for isVerb to accept 2nd param.
                     invalid.push(action);
                 }
             }
