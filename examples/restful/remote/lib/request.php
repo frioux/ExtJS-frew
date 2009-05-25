530936ca38bb2647d48c5d18e9d167c6603704ed
chrisscott <chrisscott@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-05-25 04:40:03 +0000

    add regexpressions to tear-apart path_info for controller, action, id
    git-svn-id: http://code.extjs.com/svn/ext/trunk@4030 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/restful/remote/lib/request.php
+++ examples/restful/remote/lib/request.php
@@ -4,7 +4,7 @@
  * @class Request
  */
 class Request {
-    public $restful, $method, $id, $params;
+    public $restful, $method, $controller, $action, $id, $params;
     public function __construct($params) {
         $this->restful = (isset($params["restful"])) ? $params["restful"] : false;
         $this->method = $_SERVER["REQUEST_METHOD"];
@@ -29,10 +29,29 @@ class Request {
             // grab JSON data if there...
             $this->params = (isset($_REQUEST['data'])) ? json_decode(stripslashes($_REQUEST['data']), true) : null;
         }
-        // grab id from path if exists... (eg: /users/69)
+        // parse path info
         if (isset($_SERVER["PATH_INFO"])){
-            $id = array_pop(split('/', $_SERVER["PATH_INFO"]));
-            $this->id = (is_numeric($id)) ? $id : null;
+            $cai = '/^\/([a-z]+\w)\/([a-z]+\w)\/([0-9]+\w)$/';  // /controller/action/id
+            $ca =  '/^\/([a-z]+\w)\/([a-z]+\w)$/';              // /controller/action
+            $ci = '/^\/([a-z]+\w)\/([0-9]+\w)$/';               // /controller/id
+            $c =  '/^\/([a-z]+)$/';                             // /controller
+            $i =  '/^\/([0-9]+)$/';                             // /id
+            $matches = array();
+            if (preg_match($cai, $_SERVER["PATH_INFO"], $matches)) {
+                $this->controller = $matches[1];
+                $this->action = $matches[2];
+                $this->id = $matches[3];
+            } else if (preg_match($ca, $_SERVER["PATH_INFO"], $matches)) {
+                $this->controller = $matches[1];
+                $this->action = $matches[2];
+            } else if (preg_match($ci, $_SERVER["PATH_INFO"], $matches)) {
+                $this->controller = $matches[1];
+                $this->id = $matches[2];
+            } else if (preg_match($c, $_SERVER["PATH_INFO"], $matches)) {
+                $this->controller = $matches[1];
+            } else if (preg_match($i, $_SERVER["PATH_INFO"], $matches)) {
+                $this->id = $matches[1];
+            }
         }
     }
 }
