28d14e375b3e0cb89ebd94659907f8b6e3cf4c94
nwhite <nwhite@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-06-02 08:19:27 +0000

    Fix doc typo.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@4154 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/form/Form.js
+++ src/widgets/form/Form.js
@@ -95,7 +95,7 @@ Ext.FormPanel = Ext.extend(Ext.Panel, {
      * @cfg {Boolean} monitorValid If <tt>true</tt>, the form monitors its valid state <b>client-side</b> and
      * regularly fires the {@link #clientvalidation} event passing that state.<br>
      * <p>When monitoring valid state, the FormPanel enables/disables any of its configured
-     * {@link #button}s which have been configured with <tt>formBind: true</tt> depending
+     * {@link #buttons} which have been configured with <code>formBind: true</code> depending
      * on whether the {@link Ext.form.BasicForm#isValid form is valid} or not. Defaults to <tt>false</tt></p>
      */
     monitorValid : false,
