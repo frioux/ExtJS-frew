3c38e45f61d4f2442e6d46faa73226019c9d6cc4
nwhite <nwhite@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-06-10 17:40:25 +0000

    Correct relative links in Panel header section describing its use as a Container.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@4295 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/Panel.js
+++ src/widgets/Panel.js
@@ -4,10 +4,10 @@
  * <p>Panel is a container that has specific functionality and structural components that make
  * it the perfect building block for application-oriented user interfaces.</p>
  * <p>Panels are, by virtue of their inheritance from {@link Ext.Container}, capable
- * of being configured with a {@link layout}, and containing child Components.</p>
- * <p>When either specifying child {@link #items} of a Panel, or dynamically {@link #add adding} Components
+ * of being configured with a {@link Ext.Container#layout layout}, and containing child Components.</p>
+ * <p>When either specifying child {@link Ext.Component#items items} of a Panel, or dynamically {@link Ext.Container#add adding} Components
  * to a Panel, remember to consider how you wish the Panel to arrange those child elements, and whether
- * those child elements need to be sized using one of Ext's built-in <tt><b>{@link #layout}</b></tt> schemes. By
+ * those child elements need to be sized using one of Ext's built-in <tt><b>{@link Ext.Container#layout layout}</b></tt> schemes. By
  * default, Panels use the {@link Ext.layout.ContainerLayout ContainerLayout} scheme. This simply renders
  * child components, appending them one after the other inside the Container, and <b>does not apply any sizing</b>
  * at all.</p>
