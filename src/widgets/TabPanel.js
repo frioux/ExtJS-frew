05b9d614f1697e35400d459c5593ff3520871306
mjlecomte <mjlecomte@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-06-12 16:35:23 +0000

    Docs. Clarify string and number. Defer detailed documentation to MixedCollection.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@4341 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/TabPanel.js
+++ src/widgets/TabPanel.js
@@ -749,7 +749,15 @@ new Ext.TabPanel({
     /**
      * Sets the specified tab as the active tab. This method fires the {@link #beforetabchange} event which
      * can <tt>return false</tt> to cancel the tab change.
-     * @param {String/Panel} tab The id or tab Panel to activate
+     * @param {String/Number} item 
+     * The id or tab Panel to activate. This parameter may be any of the following:
+     * <div><ul class="mdetail-params">
+     * <li>a <b><tt>String</tt></b> : representing the <code>{@link Ext.Component#itemId itemId}</code> 
+     * or <code>{@link Ext.Component#id id}</code> of the child component </li>
+     * <li>a <b><tt>Number</tt></b> : representing the position of the child component
+     * within the <code>{@link #items}</code> <b>property</b></li>
+     * </ul></div>
+     * <p>For additional information see {@link Ext.util.MixedCollection#get}.
      */
     setActiveTab : function(item){
         item = this.getComponent(item);
