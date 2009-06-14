dda1d35500a37061b45d6f0b8d026c2d46c670f6
mjlecomte <mjlecomte@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-06-14 18:38:03 +0000

    Example.  Update TableGrid class and associated example for ux namespace.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@4373 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/grid/from-markup.js
+++ examples/grid/from-markup.js
@@ -1,12 +1,14 @@
-Ext.onReady(function() {
-  var btn = Ext.get("create-grid");
-  btn.on("click", function(){
-    btn.dom.disabled = true;
-
-    // create the grid
-    var grid = new Ext.ux.TableGrid("the-table", {
-      stripeRows: true // stripe alternate rows
-    });
-    grid.render();
-  }, false, {single:true}); // run once
+Ext.onReady(function(){
+    var btn = Ext.get("create-grid");
+    btn.on("click", function(){
+        btn.dom.disabled = true;
+        
+        // create the grid
+        var grid = new Ext.ux.grid.TableGrid("the-table", {
+            stripeRows: true // stripe alternate rows
+        });
+        grid.render();
+    }, false, {
+        single: true
+    }); // run once
 });
