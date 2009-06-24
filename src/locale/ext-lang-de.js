72f99bdb7680afa65650aa6aeef61b69894afe1a
mjlecomte <mjlecomte@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-06-24 18:43:41 +0000

    Locale (ext-lang-de). Update from contributions by patrick 06-24-2009.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@4543 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/locale/ext-lang-de.js
+++ src/locale/ext-lang-de.js
@@ -284,6 +284,25 @@ if(Ext.grid.PropertyColumnModel){
   });
 }
 
+if(Ext.grid.BooleanColumn){
+   Ext.apply(Ext.grid.BooleanColumn.prototype, {
+      trueText  : "wahr",
+      falseText : "falsch"
+   });
+}
+
+if(Ext.grid.NumberColumn){
+    Ext.apply(Ext.grid.NumberColumn.prototype, {
+        format : '0.000,00'
+    });
+}
+
+if(Ext.grid.DateColumn){
+    Ext.apply(Ext.grid.DateColumn.prototype, {
+        format : 'd.m.Y'
+    });
+}
+
 if(Ext.layout.BorderLayout && Ext.layout.BorderLayout.SplitRegion){
   Ext.apply(Ext.layout.BorderLayout.SplitRegion.prototype, {
     splitTip            : "Ziehen, um Größe zu ändern.",
