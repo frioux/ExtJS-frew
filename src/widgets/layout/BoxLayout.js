0856b20e8787044c6834e1741e88a2316a819aa2
mjlecomte <mjlecomte@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-06-24 18:18:13 +0000

    Docs. Elaborate about margins.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@4542 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/layout/BoxLayout.js
+++ src/widgets/layout/BoxLayout.js
@@ -6,9 +6,32 @@
 Ext.layout.BoxLayout = Ext.extend(Ext.layout.ContainerLayout, {
     /**
      * @cfg {Object} defaultMargins
-     * If the individual contained items do not have a <tt>margins</tt> property specified, the margins
-     * from this object literal representing the default margins will be applied to each item. Defaults
-     * to <tt>{left:0,top:0,right:0,bottom:0}</tt>.
+     * <p>If the individual contained items do not have a <tt>margins</tt>
+     * property specified, the default margins from this property will be
+     * applied to each item.</p>
+     * <br><p>This property may be specified as an object containing margins
+     * to apply in the format:</p><pre><code>
+{
+    top: (top margin),
+    right: (right margin),
+    bottom: (bottom margin),
+    left: (left margin)
+}</code></pre>
+     * <p>This property may also be specified as a string containing
+     * space-separated, numeric margin values. The order of the sides associated
+     * with each value matches the way CSS processes margin values:</p>
+     * <div class="mdetail-params"><ul>
+     * <li>If there is only one value, it applies to all sides.</li>
+     * <li>If there are two values, the top and bottom borders are set to the
+     * first value and the right and left are set to the second.</li>
+     * <li>If there are three values, the top is set to the first value, the left
+     * and right are set to the second, and the bottom is set to the third.</li>
+     * <li>If there are four values, they apply to the top, right, bottom, and
+     * left, respectively.</li>
+     * </ul></div>
+     * <p>Defaults to:</p><pre><code>
+     * {top:0, right:0, bottom:0, left:0}
+     * </code></pre>
      */
     defaultMargins : {left:0,top:0,right:0,bottom:0},
     /**
