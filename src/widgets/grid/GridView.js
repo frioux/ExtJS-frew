7697039411fb1bba195e2c10ad32e0f0292e5e14
evant <evant@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-07-02 15:20:08 +0000

    Allow syncFocusEl to function correctly if all columns are hidden. At the moment it would attempt to an access a column that doesn't exist.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@4676 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/grid/GridView.js
+++ src/widgets/grid/GridView.js
@@ -943,9 +943,12 @@ viewConfig: {
         }
         col = (col !== undefined ? col : 0);
 
-        var rowEl = this.getRow(row), cellEl;
+        var rowEl = this.getRow(row),
+            cm = this.cm,
+            colCount = cm.getColumnCount(),
+            cellEl;
         if(!(hscroll === false && col === 0)){
-            while(this.cm.isHidden(col)){
+            while(col < colCount && cm.isHidden(col)){
                 col++;
             }
             cellEl = this.getCell(row, col);
