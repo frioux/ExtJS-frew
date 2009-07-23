41f96cd996bf9b69ba6f8d9465d6476c39f273f8
evant <evant@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-07-23 01:16:16 +0000

    [type]:   fix [module]: widgets [id]:     #t-75161 [desc]:   Check the record is active in the store before doing an update.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@4893 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/DataView.js
+++ src/widgets/DataView.js
@@ -311,16 +311,18 @@ Ext.DataView = Ext.extend(Ext.BoxComponent, {
     // private
     onUpdate : function(ds, record){
         var index = this.store.indexOf(record);
-        var sel = this.isSelected(index);
-        var original = this.all.elements[index];
-        var node = this.bufferRender([record], index)[0];
+        if(index > -1){
+            var sel = this.isSelected(index);
+            var original = this.all.elements[index];
+            var node = this.bufferRender([record], index)[0];
 
-        this.all.replaceElement(index, node, true);
-        if(sel){
-            this.selected.replaceElement(original, node);
-            this.all.item(index).addClass(this.selectedClass);
+            this.all.replaceElement(index, node, true);
+            if(sel){
+                this.selected.replaceElement(original, node);
+                this.all.item(index).addClass(this.selectedClass);
+            }
+            this.updateIndexes(index, index);
         }
-        this.updateIndexes(index, index);
     },
 
     // private
