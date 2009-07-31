f4fc1555be62b7ddcad50000c8990b027625c33e
mystix <mystix@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-07-31 04:22:37 +0000

    [type]:   fix [module]: util [id]:     #t-76206 [desc]:   call setDate(1) first on the cloned date to prevent problems resulting from date rollover when calling setMonth() [priv]:   -
    git-svn-id: http://code.extjs.com/svn/ext/trunk@4946 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/util/Date.js
+++ src/util/Date.js
@@ -972,12 +972,12 @@ Ext.apply(Date.prototype, {
      * @return {Number} 0 to 364 (365 in leap years).
      */
     getDayOfYear: function() {
-        var i = 0,
-            num = 0,
+        var num = 0,
             d = this.clone(),
-            m = this.getMonth();
+            m = this.getMonth(),
+            i;
 
-        for (i = 0, d.setMonth(0); i < m; d.setMonth(++i)) {
+        for (i = 0, d.setDate(1), d.setMonth(0); i < m; d.setMonth(++i)) {
             num += d.getDaysInMonth();
         }
         return num + this.getDate() - 1;
