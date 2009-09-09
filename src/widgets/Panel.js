3a36962ef017fb0a9deaaaa7bd4265fb97cad982
nwhite <nwhite@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-09-09 09:50:23 +0000

    [type]:   doc [module]: widgets [id]:     #t-65644 [desc]:   Fix param desc for tools handler.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@5310 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/Panel.js
+++ src/widgets/Panel.js
@@ -353,7 +353,7 @@ var w = new Ext.Window({
      * <li><b>event</b> : Ext.EventObject<div class="sub-desc">The click event.</div></li>
      * <li><b>toolEl</b> : Ext.Element<div class="sub-desc">The tool Element.</div></li>
      * <li><b>panel</b> : Ext.Panel<div class="sub-desc">The host Panel</div></li>
-     * <li><b>tc</b> : Ext.Panel<div class="sub-desc">The tool configuration object</div></li>
+     * <li><b>tc</b> : Object<div class="sub-desc">The tool configuration object</div></li>
      * </ul></div></li>
      * <li><b>stopEvent</b> : Boolean<div class="sub-desc">Defaults to true. Specify as false to allow click event to propagate.</div></li>
      * <li><b>scope</b> : Object<div class="sub-desc">The scope in which to call the handler.</div></li>
