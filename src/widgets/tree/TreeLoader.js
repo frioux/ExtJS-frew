b5022c5d68b316d97588ee2dd84e098240430649
evant <evant@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-10-01 14:51:34 +0000

    [type]:   fix [module]: widgets [id]:     refs #278 [desc]:   Added a fix so that createNode doesn't overwrite the loader if one is passed in the configuration.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@5440 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/tree/TreeLoader.js
+++ src/widgets/tree/TreeLoader.js
@@ -291,7 +291,7 @@ new Ext.tree.TreePanel({
         if(this.baseAttrs){
             Ext.applyIf(attr, this.baseAttrs);
         }
-        if(this.applyLoader !== false){
+        if(this.applyLoader !== false && !attr.loader){
             attr.loader = this;
         }
         if(Ext.isString(attr.uiProvider)){
