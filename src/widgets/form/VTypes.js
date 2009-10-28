d78c9b3d51981e436ba8233b8ce74f6882b47f33
evant <evant@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-10-28 05:16:08 +0000

    [type]:   fix|changed [module]: widgets [id]:     refs #326 [desc]:   Update email regex to allow for 6 character TLDs
    git-svn-id: http://code.extjs.com/svn/ext/trunk@5560 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/form/VTypes.js
+++ src/widgets/form/VTypes.js
@@ -36,7 +36,7 @@ Ext.form.VTypes = function(){
     // closure these in so they are only created once.
     var alpha = /^[a-zA-Z_]+$/,
         alphanum = /^[a-zA-Z0-9_]+$/,
-        email = /^(\w+)([\-+.][\w]+)*@(\w[\-\w]*\.){1,5}([A-Za-z]){2,4}$/,
+        email = /^(\w+)([\-+.][\w]+)*@(\w[\-\w]*\.){1,5}([A-Za-z]){2,6}$/,
         url = /(((^https?)|(^ftp)):\/\/([\-\w]+\.)+\w{2,3}(\/[%\-\w]+(\.\w{2,})?)*(([\w\-\.\?\\\/+@&#;`~=%!]*)(\.\w{2,})?)*\/?)/i;
 
     // All these messages and functions are configurable
