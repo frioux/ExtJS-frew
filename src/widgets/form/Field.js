169cb67b39a2dacad922098cb6f5afd32b8e9c1b
evant <evant@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-11-04 06:26:39 +0000

    [type]:   fix [module]: form [id]:     refs #329 [desc]:   Added a fix to ensure that validation doesn't always occur on blur.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@5585 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/widgets/form/Field.js
+++ src/widgets/form/Field.js
@@ -361,7 +361,7 @@ var form = new Ext.form.FormPanel({
             this.el.removeClass(this.focusClass);
         }
         this.hasFocus = false;
-        if(this.validationEvent !== false && (this.validateOnBlur || this.validationEvent != 'blur')){
+        if(this.validationEvent !== false && (this.validateOnBlur || this.validationEvent == 'blur')){
             this.validate();
         }
         var v = this.getValue();
