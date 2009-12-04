5c737c30c87fd3c0f3288129acd978f4046be052
javins <javins@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-12-04 20:56:12 +0000

    [type]: fix [module]: example [id]: [desc]: Fix example for IE. Change layoutConfig to autoWidth: false
    git-svn-id: http://code.extjs.com/svn/ext/trunk@5708 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/direct/direct-form.js
+++ examples/direct/direct-form.js
@@ -6,10 +6,10 @@ Ext.onReady(function(){
     // Slow the buffering down from the default of 10ms to 100ms
     Ext.app.REMOTING_API.enableBuffer = 100;
     Ext.Direct.addProvider(Ext.app.REMOTING_API);
-    
+
     // provide feedback for any errors
     Ext.QuickTips.init();
-    
+
     var basicInfo = new Ext.form.FormPanel({
         // configs for FormPanel
         title: 'Basic Information',
@@ -21,7 +21,7 @@ Ext.onReady(function(){
                 basicInfo.getForm().submit({
                     params: {
                         foo: 'bar',
-                        uid: 34 
+                        uid: 34
                     }
                 });
             }
@@ -36,12 +36,12 @@ Ext.onReady(function(){
         },{
             fieldLabel: 'Email',
             msgTarget: 'side',
-            name: 'email'        
+            name: 'email'
         },{
             fieldLabel: 'Company',
             name: 'company'
         }],
-        
+
         // configs for BasicForm
         api: {
             // The server-side method to call for load() requests
@@ -49,16 +49,16 @@ Ext.onReady(function(){
             // The server-side must mark the submit handler as a 'formHandler'
             submit: Profile.updateBasicInfo
         },
-        // specify the order for the passed params    
+        // specify the order for the passed params
         paramOrder: ['uid', 'foo']
     });
-    
+
     var phoneInfo = new Ext.form.FormPanel({
         title: 'Phone Numbers',
         border: false,
         api: {
             load: Profile.getPhoneInfo
-        },    
+        },
         padding: 10,
         paramOrder: ['uid'],
         defaultType: 'textfield',
@@ -68,20 +68,20 @@ Ext.onReady(function(){
             name: 'office'
         },{
             fieldLabel: 'Cell',
-            name: 'cell'        
+            name: 'cell'
         },{
             fieldLabel: 'Home',
             name: 'home'
         }]
     });
-    
+
      var locationInfo = new Ext.form.FormPanel({
         title: 'Location Information',
         border: false,
         padding: 10,
         api: {
             load: Profile.getLocationInfo
-        },    
+        },
         paramOrder: ['uid'],
         defaultType: 'textfield',
         defaults: {anchor: '100%'},
@@ -90,7 +90,7 @@ Ext.onReady(function(){
             name: 'street'
         },{
             fieldLabel: 'City',
-            name: 'city'            
+            name: 'city'
         },{
             fieldLabel: 'State',
             name: 'state'
@@ -98,23 +98,26 @@ Ext.onReady(function(){
             fieldLabel: 'Zip',
             name: 'zip'
         }]
-    });    
+    });
 
      var accordion = new Ext.Panel({
         layout: 'accordion',
+        layoutConfig: {
+            autoWidth : false
+        },
         renderTo: Ext.getBody(),
         title: 'My Profile',
         width: 300,
-        height: 240,    
+        height: 240,
         items: [basicInfo, phoneInfo, locationInfo]
     });
-        
+
     // load the forms (notice the load requests will get batched together)
     basicInfo.getForm().load({
         // pass 2 arguments to server side getBasicInfo method (len=2)
         params: {
             foo: 'bar',
-            uid: 34 
+            uid: 34
         }
     });
 
@@ -122,7 +125,7 @@ Ext.onReady(function(){
         params: {
             uid: 5
         }
-    });    
+    });
 
     // defer this request just to simulate the request not getting batched
     // since it exceeds to configured buffer
@@ -132,8 +135,8 @@ Ext.onReady(function(){
                 uid: 5
             }
         });
-    }).defer(200);    
-    
+    }).defer(200);
+
     // rpc call
     TestAction.doEcho('sample');
 
