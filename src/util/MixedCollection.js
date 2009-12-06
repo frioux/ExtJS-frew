34df04e9f6754cc1567ab0a4cce426302a97bc87
mjlecomte <mjlecomte@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-12-06 14:14:26 +0000

    [type]:   doc [module]: util [id]: [desc]:   Remove license block (added later by builder). [priv]:
    git-svn-id: http://code.extjs.com/svn/ext/trunk@5725 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- src/util/MixedCollection.js
+++ src/util/MixedCollection.js
@@ -1,9 +1,3 @@
-/*!
- * Ext JS Library 3.0+
- * Copyright(c) 2006-2009 Ext JS, LLC
- * licensing@extjs.com
- * http://www.extjs.com/license
- */
 /**
  * @class Ext.util.MixedCollection
  * @extends Ext.util.Observable
