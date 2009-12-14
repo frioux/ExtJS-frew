6faeadbc518e432454e4ae8b9111fa6432dd6515
evant <evant@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2009-12-14 13:04:59 +0000

    [type]:   fix [module]: example [id]:     refs #383 [desc]:   Added a fix that allows multiple CheckColumns to be used in a grid.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@5777 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/ux/CheckColumn.js
+++ examples/ux/CheckColumn.js
@@ -53,7 +53,7 @@ Ext.ux.grid.CheckColumn.prototype ={
     },
 
     onMouseDown : function(e, t){
-        if(t.className && t.className.indexOf('x-grid3-cc-'+this.id) != -1){
+        if(Ext.fly(t).hasClass(this.createId())){
             e.stopEvent();
             var index = this.grid.getView().findRowIndex(t);
             var record = this.grid.store.getAt(index);
@@ -63,7 +63,11 @@ Ext.ux.grid.CheckColumn.prototype ={
 
     renderer : function(v, p, record){
         p.css += ' x-grid3-check-col-td'; 
-        return '<div class="x-grid3-check-col'+(v?'-on':'')+' x-grid3-cc-'+this.id+'">&#160;</div>';
+        return String.format('<div class="x-grid3-check-col{0} {1}">&#160;</div>', v ? '-on' : '', this.createId());
+    },
+    
+    createId : function(){
+        return 'x-grid3-cc-' + this.id;
     }
 };
 
