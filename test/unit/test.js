4f6c624fb86426e2a3fb5abae47ac490771a3d5f
espencer <espencer@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2010-02-03 18:55:22 +0000

    Added some mock test files
    git-svn-id: http://code.extjs.com/svn/ext/trunk@5984 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

---
+++ test/unit/test.js
@@ -0,0 +1,19 @@
+Ext.tests.push(new Y.Test.Case({
+  name: 'Some test case',
+  
+  testEdIsCool: function() {
+    Y.Assert.areEqual(5, 2 + 3);
+  },
+  
+  testTommyIsCrazy: function() {
+    Y.Assert.areEqual('Tommy', 'crazy');
+  },
+  
+  testAbeIsTall: function() {
+    var abe = {
+      height: 6.8
+    };
+    
+    Y.Assert.isTrue(abe.height >= 6.5);   
+  }
+}));
