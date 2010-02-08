534a4ed8617e5ab927552f27993a185cc2250ead
jayrobinson <jayrobinson@c0a82769-a6a8-4f8a-96ba-7530dc2e272d>
2010-02-08 18:54:36 +0000

    [type]: minor [desc]: Added "Offline Support" heading to Examples. Reordered some examples.
    git-svn-id: http://code.extjs.com/svn/ext/trunk@6006 c0a82769-a6a8-4f8a-96ba-7530dc2e272d

--- examples/data.js
+++ examples/data.js
@@ -9,20 +9,10 @@ Ext.samples.samplesCatalog = [{
         icon: 'feeds.gif',
         desc: 'RSS feed reader example application that features a swappable reader panel layout.'
     },{
-        text: 'Simple Tasks',
-        url:  'http://extjs.com/blog/2008/02/24/tasks2/',
-        icon: 'air.gif',
-        desc: 'Complete personal task management application example that runs on <a href="http://labs.adobe.com/technologies/air/" target="_blank">Adobe AIR</a>.'
-    },{
-        text: 'Simple Tasks',
-        url:  'tasks/tasks.html',
-        icon: 'tasks.gif',
-        desc: 'Personal task management application example that uses <a href="http://gears.google.com" target="_blank">Google Gears</a> for data storage.'
-    },{
-        text: 'Image Viewer',
-        url:  'organizer/organizer.html',
-        icon: 'organizer.gif',
-        desc: 'DataView and TreePanel example that demonstrates dragging data items from a DataView into a TreePanel.'
+        text: 'Web Desktop',
+        url:  'desktop/desktop.html',
+        icon: 'desktop.gif',
+        desc: 'Demonstrates how one could build a desktop in the browser using Ext components including a module plugin system.'
     }/*,{
         text: 'Image Organizer',
         url:  'image-organizer/index.html',
@@ -42,10 +32,23 @@ Ext.samples.samplesCatalog = [{
         desc: 'Ext JS online forums browser application.',
         status: 'modified'
     },{
-        text: 'Web Desktop',
-        url:  'desktop/desktop.html',
-        icon: 'desktop.gif',
-        desc: 'Demonstrates how one could build a desktop in the browser using Ext components including a module plugin system.'
+        text: 'Image Viewer',
+        url:  'organizer/organizer.html',
+        icon: 'organizer.gif',
+        desc: 'DataView and TreePanel example that demonstrates dragging data items from a DataView into a TreePanel.'
+    }]
+},{
+    title: 'Offline Support',
+    samples: [{
+        text: 'Simple Tasks',
+        url:  'tasks/tasks.html',
+        icon: 'tasks.gif',
+        desc: 'Personal task management application example that uses <a href="http://gears.google.com" target="_blank">Google Gears</a> for data storage.'
+    },{
+        text: 'Simple Tasks',
+        url:  'http://extjs.com/blog/2008/02/24/tasks2/',
+        icon: 'air.gif',
+        desc: 'Complete personal task management application example that runs on <a href="http://labs.adobe.com/technologies/air/" target="_blank">Adobe AIR</a>.'
     }]
 },{
     title: 'Accessibility',
